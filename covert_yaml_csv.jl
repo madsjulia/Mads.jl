4ea1341081702b44ba29685d72ffeb73f0305dbd
monty <velimir.vesselinov@gmail.com>
2015-07-18 11:22:04 -0600

    covert_yaml_csv.jl added

---
+++ covert_yaml_csv.jl
@@ -0,0 +1,12 @@
+for n in keys(m["Wells"])
+	x = m["Wells"]["$n"]["x"]
+	y = m["Wells"]["$n"]["y"]
+	z0 = m["Wells"]["$n"]["z0"]
+	z1 = m["Wells"]["$n"]["z1"]
+	o = m["Wells"]["$n"]["obs"]
+	for i in 1:length(o)
+		c = o[i][i]["c"]
+		t = o[i][i]["t"]
+		println("$n, $x, $y, $z0, $t, $c")
+	end
+end
