36409780d7586eb73dbab1e3df808d49aac763e9
monty <velimir.vesselinov@gmail.com>
2015-08-11 09:11:08 -0600

    optimization_linear_problem+template.jl added

---
+++ tests/optimization_linear_problem+template.jl
@@ -0,0 +1,8 @@
+using Mads
+
+Mads.madsinfo("TEST Levenberg-Marquardt optimization of an internal call problem using templates and Julia script to parse the model outputs:")
+problemdir = string((dirname(Base.source_path())))
+Mads.madsinfo("""Problem directory: $(problemdir)""")
+mdinternal = Mads.loadyamlmadsfile(problemdir*"/test-internal-linearmodel+template.mads")
+results = Mads.calibrate(mdinternal)
+Mads.madsoutput("""$results\n""")
--- tests/optimization_linear_problem.jl
+++ tests/optimization_linear_problem.jl
@@ -6,8 +6,3 @@ Mads.madsinfo("""Problem directory: $(problemdir)""")
 mdinternal = Mads.loadyamlmadsfile(problemdir*"/test-internal-linearmodel.mads")
 results = Mads.calibrate(mdinternal)
 Mads.madsoutput("""$results\n""")
-
-Mads.madsinfo("TEST Levenberg-Marquardt optimization of an internal call problem using templates and Julia script to parse the model outputs:")
-mdinternal = Mads.loadyamlmadsfile(problemdir*"/test-internal-linearmodel+template.mads")
-results = Mads.calibrate(mdinternal)
-Mads.madsoutput("""$results\n""")
