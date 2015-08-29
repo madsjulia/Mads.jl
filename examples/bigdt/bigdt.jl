c1f03e27390a9f575b7d1da0fe751a0b8aea37ac
monty <velimir.vesselinov@gmail.com>
2015-08-29 13:25:55 -0600

    various changes

--- examples/bigdt/bigdt.jl
+++ examples/bigdt/bigdt.jl
@@ -3,7 +3,7 @@ import Mads
 problemdir = Mads.getmadsdir()
 md = Mads.loadyamlmadsfile(problemdir * "source_termination.mads")
 Mads.plotmadsproblem(md)
-maxfailureprobs, horizons = Mads.dobigdt(md, 100; hakunamatata=0.8, numlikelihoods=5)
-Mads.plotrobustnesscurves(md, maxfailureprobs, horizons)
+bigdtresults = Mads.dobigdt(md, 100; maxHorizon=0.8, numlikelihoods=5)
+Mads.plotrobustnesscurves(md, bigdtresults)
 # mcvalues = Mads.paramarray2dict(md, chain.samples) # convert the parameters in the chain samples to a parameter dictionary of arrays
 # Mads.spaghettiplot(md, mcvalues) #TODO work with Wells class
--- src/MadsBIG.jl
+++ src/MadsBIG.jl
@@ -1,15 +1,20 @@
 import BIGUQ
+import ProgressMeter
 
 function makebigdt(madsdata::Associative, choice::Associative)
 	return makebigdt!(deepcopy(madsdata), choice)
 end
 
 function makebigdt!(madsdata::Associative, choice::Associative)
+	Mads.info("Decision parameters:")
 	for paramname in keys(choice["Parameters"])
 		if madsdata["Parameters"][paramname]["type"] == "opt"
-			error("A decision parameter, $paramname, is also an \"opt\" parameter.")
+			Mads.err("Decision parameter, $paramname, is also an adjustable (type = \"opt\") parameter.")
+			throw("MADS input error; MADS quits!")
 		end
-		madsdata["Parameters"][paramname]["init"] = choice["Parameters"][paramname]
+		c = choice["Parameters"][paramname]
+		madsdata["Parameters"][paramname]["init"] = c
+		Mads.info("Decision parameter $paramname set to $c.")
 	end
 	makeloglikelihood = makemakearrayconditionalloglikelihood(madsdata)
 	logprior = makearrayfunction(madsdata, makelogprior(madsdata))
@@ -49,7 +54,7 @@ function makebigdt!(madsdata::Associative, choice::Associative)
 	return BIGUQ.BigDT(makeloglikelihood, logprior, nominalparams, likelihoodparamsmin, likelihoodparamsmax, performancegoalsatisfied)
 end
 
-function dobigdt(madsdata::Associative, nummodelruns::Int; numhorizons::Int=100, hakunamatata::Real=3., numlikelihoods::Int=25)
+function dobigdt(madsdata::Associative, nummodelruns::Int; numhorizons::Int=100, maxHorizon::Real=3., numlikelihoods::Int=25)
 	parametersamples = parametersample(madsdata, nummodelruns)
 	optparamkeys = getoptparamkeys(madsdata)
 	modelparams = Array(Float64, length(parametersamples), nummodelruns)
@@ -61,11 +66,13 @@ function dobigdt(madsdata::Associative, nummodelruns::Int; numhorizons::Int=100,
 	getfailureprobs = BIGUQ.makegetfailureprobabilities_mc(modelparams)
 	maxfailureprobs = Array(Float64, numhorizons, length(madsdata["Choices"]))
 	local horizons::Array{Float64, 1}
-	for i = 1:length(madsdata["Choices"])
+	Mads.info("Choices:")
+	@showprogress 1 "Computing ... " for i = 1:length(madsdata["Choices"])
+		Mads.info("Choice #$i: $(madsdata["Choices"][i]["name"])")
 		bigdt = makebigdt(madsdata, madsdata["Choices"][i])
-		maxfailureprobs[:, i], horizons, badlikelihoodparams = BIGUQ.getrobustnesscurve(bigdt, hakunamatata, numlikelihoods; getfailureprobfnct=getfailureprobs, numhorizons=numhorizons)
+		maxfailureprobs[:, i], horizons, badlikelihoodparams = BIGUQ.getrobustnesscurve(bigdt, maxHorizon, numlikelihoods; getfailureprobfnct=getfailureprobs, numhorizons=numhorizons)
 	end
-	return maxfailureprobs, horizons
+	[ "maxfailureprobs" => maxfailureprobs, "horizons" => horizons ]
 end
 
 function makemakearrayconditionalloglikelihood(madsdata::Associative)
@@ -75,20 +82,22 @@ function makemakearrayconditionalloglikelihood(madsdata::Associative)
 	end
 end
 
-function plotrobustnesscurves(madsdata::Associative, maxfailureprobs::Matrix, horizons::Vector; filename="", format="")
+function plotrobustnesscurves(madsdata::Associative, bigdtresults::Dict; filename="", format="")
+	maxfailureprobs = bigdtresults["maxfailureprobs"]
+	horizons = bigdtresults["horizons"]
 	if filename == ""
-		rootname = Mads.madsrootname(madsdata)
+		rootname = Mads.getmadsrootname(madsdata)
 		filename =  rootname * "-robustness"
 	end
 	filename, format = setimagefileformat(filename, format)
 	layers = Array(Any, size(maxfailureprobs, 2))
-	df = DataFrame(horizon=[], maxfailureprob=[], Choice=[])
+	df = DataFrame(horizon=[], maxfailureprob=[], Choices=[])
 	for i = 1:size(maxfailureprobs, 2)
-		df = vcat(df, DataFrame(horizon=horizons, maxfailureprob=maxfailureprobs[:, i], Choice=madsdata["Choices"][i]["name"]))
+		df = vcat(df, DataFrame(horizon=horizons, maxfailureprob=maxfailureprobs[:, i], Choices=madsdata["Choices"][i]["name"]))
 		#layers[i] = Gadfly.layer(x=horizons, y=maxfailureprobs[:, i], Geom.line)
 	end
-	#p = Gadfly.plot(layers..., Guide.xlabel("Horizon of uncertainty"), Guide.ylabel("Max probability of failure"))
-	p = Gadfly.plot(df, x="horizon", y="maxfailureprob", color="Choice", Geom.line, Guide.xlabel("Horizon of uncertainty"), Guide.ylabel("Max probability of failure"), Scale.color_discrete_manual(["red" "blue" "green" "cyan" "magenta" "yellow"]...))
+	#p = Gadfly.plot(layers..., Guide.xlabel("Horizon of uncertainty"), Guide.ylabel("Maximum probability of failure"))
+	p = Gadfly.plot(df, x="horizon", y="maxfailureprob", color="Choices", Geom.line, Guide.xlabel("Horizon of uncertainty"), Guide.ylabel("Maximum probability of failure"), Scale.color_discrete_manual(["red" "blue" "green" "cyan" "magenta" "yellow"]...))
 	Gadfly.draw(eval(Gadfly.symbol(format))(filename, 4Gadfly.inch, 3Gadfly.inch), p)
-	return nothing
+	p
 end
