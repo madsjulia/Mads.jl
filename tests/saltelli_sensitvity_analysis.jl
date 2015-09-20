310dd5ef03b000f0e69dc8025cdea208e9839021
neda <neda.vesselinova@gmail.com>
2015-09-20 10:37:55 -0600

    saltelli_sensitvity_analysis.jl fixes

--- tests/saltelli_sensitvity_analysis.jl
+++ tests/saltelli_sensitvity_analysis.jl
@@ -1,19 +1,19 @@
 using Mads
 
-# Saltelli senstivity analysis tests
+problemdir = Mads.getmadsdir()
 Mads.madsinfo("TEST Saltelli senstivity analysis: Sobol test:")
-mdsobol = Mads.loadyamlmadsfile("test-sobol.mads")
-results = Mads.saltelli(mdsobol,N=int(1e2))
-Mads.saltelliprintresults(mdsobol,results)
+mdsobol = Mads.loadyamlmadsfile(problemdir * "test-sobol.mads")
+results = Mads.saltelli(mdsobol, N=int(1e3))
+Mads.printSAresults(mdsobol, results)
 Mads.madsinfo("TEST Saltelli senstivity analysis: Linear problem:")
-mdsaltelli = Mads.loadyamlmadsfile("test-saltelli.mads")
+mdsaltelli = Mads.loadyamlmadsfile(problemdir * "test-saltelli.mads")
 results = Mads.saltelli(mdsaltelli) # Fast
-Mads.saltelliprintresults(mdsaltelli,results)
+Mads.printSAresults(mdsaltelli, results)
 # Mads.madsinfo("TEST Saltelli senstivity analysis (brute force): Sobol test:") # TODO Brute force needs to be fixed
-# mdsobol = Mads.loadyamlmadsfile("tests/test-sobol.mads")
+# mdsobol = Mads.loadyamlmadsfile(problemdir * "test-sobol.mads")
 #results = Mads.saltellibrute(mdsobol) # Slow
 #Mads.saltelliprintresults2(mdsobol,results)
 # Mads.madsinfo("TEST Saltelli senstivity analysis (brute force): Linear problem:")
-# mdsaltelli = Mads.loadyamlmadsfile("tests/test-saltelli.mads")
+# mdsaltelli = Mads.loadyamlmadsfile(problemdir * "test-saltelli.mads")
 #results = Mads.saltellibrute(mdsaltelli) # Slow
 #Mads.saltelliprintresults2(mdsaltelli,results)
