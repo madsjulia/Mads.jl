60d406b5b08ab2cf50d740600465cdb50d9b5daf
monty <velimir.vesselinov@gmail.com>
2015-10-11 11:50:05 -0600

    examples/learn_julia added

---
+++ examples/learn_julia/polimorphism.jl
@@ -0,0 +1,9 @@
+# function polymorphism
+polyf(x)=x^3
+polyf(3)
+polyf(3.1415)
+polyf("ho ")
+
+z=rand(4,4)
+# call the function on matrix
+polyf(z)
---
+++ examples/learn_julia/statistics.jl
@@ -0,0 +1,23 @@
+using Distributions
+using Gadfly
+
+# create random vector with normal distribution
+dist = Distributions.Uniform(1,2)
+dist = Distributions.LogNormal(1,4)
+dist = Distributions.Levy(3,4)
+dist = Distributions.Beta(10,3)
+dist = Distributions.Normal(500, 30)
+n=rand(dist, 1000)
+
+mean(n)
+std(n)
+var(n)
+skewness(n)
+kurtosis(n)
+Distributions.moment(n,3)
+
+# plot the histogram using Gadfly
+Gadfly.plot(x=n, Gadfly.Geom.histogram(bincount=40))
+Gadfly.plot(x=n, Gadfly.Geom.histogram(bincount=40), Gadfly.Scale.x_log10)
+
+
