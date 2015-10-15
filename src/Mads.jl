4c2e4a09874d11793d257adf482259d9c3046537
monty <velimir.vesselinov@gmail.com>
2015-10-15 13:09:02 -0600

    minor

--- src/Mads.jl
+++ src/Mads.jl
@@ -85,16 +85,22 @@ function resetmodelruns()
 	global modelruns = 0
 end
 
-@doc "Set number of processors" ->
-function setnprocs(np)
+@doc "Set number of processors / threads" ->
+function setnprocs(np, nt)
 	n = np - nprocs()
 	if n > 0
 		addprocs(n)
 	elseif n < 0
 		rmprocs(workers()[end+n+1:end])
 	end
+	blas_set_num_threads(nt)
 	sleep(1)
 	madsoutput("Number of processors is $(nprocs()) $(workers())\n")
+end
+
+@doc "Set number of processors" ->
+function setnprocs(np)
+	setnprocs(np, np)
 end
 
 @doc "Save calibration results" ->
