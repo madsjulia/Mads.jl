0c76eec2292cfc0a6b21ea4f70d30f3eb4d24605
monty <velimir.vesselinov@gmail.com>
2015-10-19 20:11:56 -0600

    more added

--- examples/learn_julia/juliatutorial.jl
+++ examples/learn_julia/juliatutorial.jl
@@ -4,36 +4,38 @@
 srand(1)
 C = randn(5,5)
 C[:]
+reshape(C[:], 5, 5)
 
 ## Arrays are passed by reference, no clearing variables
 B = zeros(5)
 A = B
-B[3]=47
+B[3] = 42
 A
 
-A = 0
+# Chinease can be used
+朱莉婭 = 5
+朱莉婭 * 4
 whos()
 workspace()
 whos()
 
 ## Function naming convention, timing
-function addones!(x::Vector,z::Vector)
+function addones!(x::Vector, z::Vector)
 	z = x + ones(length(x))
 	x = x - ones(length(x))
-	return x,z
+	return x, z
 end
 
 function addones(x::Vector)
-	n = length(x)
-	z = zeros(n)
-	z = x + ones(n)
+	z = similar(y)
+	z = x + ones(length(x))
 end
 
-x = 3*ones(5)
+x = 3 * ones(5)
 y = x
 z = zeros(5)
 
-@time x, z = addones!(x,z)
+@time x, z = addones!(x, z)
 @time z2 = addones(y)
 
 #Try it again
@@ -54,51 +56,51 @@ f(4.5)
 g(x) = x^3
 g(4.5)
 g(4)
-g("Yo")
+g("Ho ")
 
 ## Linear Algebra fun(ctions)!
 srand(0)
-A = rand(5,5)
-B = A*A'
+A = rand(5, 5)
+B = A * A'
 @assert issym(B)
 @assert isposdef(B)
 
 # what does assert do? how can we quickly look it up?
 chol(B)
-L = chol(B,:L)
-U = chol(B,:U)
+L = chol(B, :L)
+U = chol(B, :U)
 
 W,S,V = svd(B)
 (),S2 = svd(A)
 
-## Plotting fun
+## PyPlotting
 using PyPlot
 
-plot(S2)
-figure()
-loglog(S2,linewidth=3,"go--")
-close("all")
+PyPlot.plot(S2)
+PyPlot.figure()
+PyPlot.loglog(S2,linewidth=3,"go--")
+PyPlot.close("all")
 
-figure()
-plot(S2,label = "sv's of A",color="green", linestyle="dashed", marker="o",markersize=12)
-plot(S,label = "sv's of B",color="magenta", linestyle="dashed", marker="o",markersize=12)
-legend()
+PyPlot.figure()
+PyPlot.plot(S2 ,label = "sv's of A",color="green", linestyle="dashed", marker="o",markersize=12)
+PyPlot.plot(S, label = "sv's of B",color="magenta", linestyle="dashed", marker="o",markersize=12)
+PyPlot.legend()
 
-figure()
-x=linspace(0,5,100)
-plot(sin(x))
-plot(cos(x))
-legend(["sin","cos"],loc="lower left")
+PyPlot.figure()
+x=0:0.1:25
+PyPlot.plot(x, sin(x))
+PyPlot.plot(y, cos(x))
+PyPlot.legend(["sin","cos"],loc="lower left")
 
 a = 47
-title("Title, a = $(a)"), xlabel("the x-axis label"), ylabel ("the y-axislabel")
+PyPlot.title("Title, a = $(a)"), xlabel("the x-axis label"), ylabel ("the y-axislabel")
 
-figure()
-title("A title with latex,\$\\alpha + \\beta\$, a = $(a)")
+PyPlot.figure()
+PyPlot.title("A title with latex,\$\\alpha + \\beta\$, a = $(a)")
 
 mypath = pwd()
-savefig(mypath * "/deleteme.pdf")
-savefig("deletemetoo.eps")
+PyPlot.savefig(mypath * "/deleteme.pdf")
+PyPlot.savefig("deletemetoo.eps")
 
 ## saving data
 using JLD
---
+++ examples/learn_julia/plot2D.jl
@@ -0,0 +1,32 @@
+using Gadfly
+using PyPlot
+
+srand(100)
+x = 1:10
+y = rand(10)
+
+Gadfly.plot(x=x, y=y)
+Gadfly.plot(x=x, y=y, Geom.point, Geom.line)
+Gadfly.plot(x=x, y=2.^y,
+     Scale.y_sqrt, Geom.point, Geom.smooth,
+     Guide.xlabel("Time"), Guide.ylabel("Response"), Guide.title("Training"))
+func_plot(x)=sin(x)+sqrt(x)
+Gadfly.plot([sin, cos, sqrt, func_plot], 0, 25)
+
+using PyPlot
+PyPlot.svg(true)
+PyPlot.clf()
+PyPlot.figure()
+PyPlot.plot(x, y)
+PyPlot.gcf()
+PyPlot.clf()
+PyPlot.figure()
+x = 0:0.1:25
+PyPlot.plot(x, sin(x))
+PyPlot.plot(x, cos(x))
+PyPlot.plot(x, sqrt(x))
+PyPlot.plot(x, func_plot(x))
+PyPlot.legend(["sin","cos","sqrt","func_plot"],loc="lower left")
+PyPlot.gcf()
+
+PyPlot.clf()
