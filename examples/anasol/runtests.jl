7662893a17bfe1dfd6264e78b653b147dce94abc
Daniel O'Malley <omalled@lanl.gov>
2016-02-22 11:49:04 -0700

    fix anasol test

--- examples/anasol/runtests.jl
+++ examples/anasol/runtests.jl
@@ -12,7 +12,7 @@ ssr = 0.
 for obskey in union(Set(keys(forward_preds)), Set(keys(good_forward_preds)))
 	ssr += (forward_preds[obskey] - good_forward_preds[obskey]) ^ 2
 end
-@test_approx_eq ssr 0.
+@test_approx_eq_eps ssr 0. 1e-8
 
 #good_forward_preds = computeconcentrations(paramdict)
 #@JLD.save "$workdir/goodresults.jld" good_forward_preds
