62db2856f5c6620b052340669185f79cbbf57805
monty <velimir.vesselinov@gmail.com>
2016-02-22 11:27:33 -0700

    wells dir

---
+++ examples/wells/optimization_wells.jl
@@ -0,0 +1,11 @@
+import Mads
+
+info("Levenberg-Marquardt optimization of an external call problem using the code WELLS ...")
+cdir = pwd()
+problemdir = string((dirname(Base.source_path()))) * "/"
+cd(problemdir)
+md = Mads.loadmadsfile("w01.mads")
+params, results = Mads.calibrate(md)
+display(params)
+cd(cdir)
+return
