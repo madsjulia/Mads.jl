90f202cef5d08c6e9237d933128deb8e723f9b5c
monty <velimir.vesselinov@gmail.com>
2016-02-28 08:45:37 -0700

    deps/build.jl now should work

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,11 @@
-info("Adding Python YAML ...")
+info("Python YAML ...")
 import PyCall
 import Conda
 try
 	@PyCall.pyimport yaml
+	info("System Python YAML ...")
 catch
 	ENV["PYTHON"]=""
 	Conda.add("yaml")
+	info("Conda Python YAML ...")
 end
