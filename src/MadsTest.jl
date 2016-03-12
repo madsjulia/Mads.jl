2852a7fe9b2c843b80a164a44749bf12848d820e
monty <velimir.vesselinov@gmail.com>
2016-03-12 13:41:06 -0700

    minor

--- src/MadsTest.jl
+++ src/MadsTest.jl
@@ -9,8 +9,8 @@ function testj()
 end
 
 "Execute Mads tests (the tests will be in parallel if processors are defined)"
-function test(test="")
-	if test == ""
+function test(testmod="")
+	if testmod == ""
 		include(Pkg.dir("Mads") * "/test/runtests.jl")
 		include(Pkg.dir("Anasol") * "/test/runtests.jl")
 		include(Pkg.dir("BIGUQ") * "/test/runtests.jl")
@@ -18,13 +18,13 @@ function test(test="")
 		include(Pkg.dir("MetaProgTools") * "/test/runtests.jl")
 		include(Pkg.dir("RobustPmap") * "/test/runtests.jl")
 	else
-		file = Pkg.dir("Mads") * "/examples/$(test)/runtests.jl"
+		file = Pkg.dir("Mads") * "/examples/$(testmod)/runtests.jl"
 		if isfile(file)
-			include(Pkg.dir("Mads") * "/examples/$(test)/runtests.jl")
-		elseif isdefined(symbol(test))
-			file = Pkg.dir(test) * "/test/runtests.jl"
+			include(file)
+		elseif isdefined(symbol(testmod))
+			file = Pkg.dir(testmod) * "/test/runtests.jl"
 			if isfile(file)
-				include(Pkg.dir(test) * "/test/runtests.jl")
+				include(file)
 			else
 				warn("Test $file is missing!")
 			end
