176d98f0f6da3976001f40a5f978dc1cd3da786e
monty <velimir.vesselinov@gmail.com>
2016-04-25 12:57:52 -0600

    minor MC fixes

--- examples/bayesian_sampling/bayesian_sampling.jl
+++ examples/bayesian_sampling/bayesian_sampling.jl
@@ -10,13 +10,14 @@ rootname = Mads.getmadsrootname(md)
 
 info("Bayesian sampling ...")
 mcmcchain = Mads.bayessampling(md; nsteps=10000, burnin=1000, thinning=1, seed=2016)
-Mads.scatterplotsamples(md, mcmcchain.value', rootname * "-bayes.svg")
-Mads.savemcmcresults(mcmcchain, rootname * "-bayes.json")
+Mads.scatterplotsamples(md, mcmcchain.value', rootname * "-bayes.png")
+Mads.savemcmcresults(mcmcchain.value', rootname * "-bayes.json")
 
 info("Parallel Bayesian sampling ...")
 mcmcchains = Mads.bayessampling(md, 2; nsteps=5000, burnin=500, thinning=1, seed=2016)
-Mads.scatterplotsamples(md, vcat(map(chain->chain.value', mcmcchains)...), rootname * "-parallel-bayes.svg")
-Mads.savemcmcresults(mcmcchains, rootname * "-parallel-bayes.json")
+chain_array = vcat(map(chain->chain.value', mcmcchains)...)
+Mads.scatterplotsamples(md, chain_array, rootname * "-parallel-bayes.png")
+Mads.savemcmcresults(chain_array, rootname * "-parallel-bayes.json")
 
 md = Mads.loadmadsfile(problemdir * "w01.mads")
 rootname = Mads.getmadsrootname(md)
@@ -32,20 +33,20 @@ Mads.setparamsinit!(md, param)
 
 info("Bayesian sampling of a contaminant transport problem (anasol) ...")
 mcmcchain = Mads.bayessampling(md; nsteps=1000, burnin=100, thinning=1, seed=2016)
-Mads.savemcmcresults(mcmcchain, rootname * "-bayes.json")
+Mads.savemcmcresults(mcmcchain.value', rootname * "-bayes.json")
 
 info("Bayesian scatter plots ...")
-Mads.scatterplotsamples(md, mcmcchain.value', rootname * "-bayes.svg")
+Mads.scatterplotsamples(md, mcmcchain.value', rootname * "-bayes.png")
 
 # convert the parameters in the chain to a parameter dictionary of arrays
 mcmcvalues = Mads.paramarray2dict(md, mcmcchain.value') 
 
 info("Posterior (Bayesian) spaghetti plots ...")
-Mads.spaghettiplots(md, mcmcvalues, keyword="posterior")
-Mads.spaghettiplot(md, mcmcvalues, keyword="posterior")
+Mads.spaghettiplots(md, mcmcvalues, keyword="posterior", format="PNG")
+Mads.spaghettiplot(md, mcmcvalues, keyword="posterior", format="PNG")
 
 info("Prior spaghetti plots ...")
-Mads.spaghettiplots(md, 100, keyword="prior")
-Mads.spaghettiplot(md, 100, keyword="prior")
+Mads.spaghettiplots(md, 100, keyword="prior", format="PNG")
+Mads.spaghettiplot(md, 100, keyword="prior", format="PNG")
 
 return
