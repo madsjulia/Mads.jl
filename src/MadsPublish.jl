e36e8746d1bb024450321b3a15d67043ac41a771
monty <velimir.vesselinov@gmail.com>
2016-08-06 08:55:15 -0600

    tag argument added

--- src/MadsPublish.jl
+++ src/MadsPublish.jl
@@ -20,11 +20,11 @@ function status(; git::Bool=true)
 	end
 end
 
-function status(testmod::AbstractString; git::Bool=true)
+function status(madsmodule::AbstractString; git::Bool=true)
 	if git
 		cwd = pwd()
-		info("Git status $(testmod) ...")
-		cd(Pkg.dir(testmod))
+		info("Git status $(madsmodule) ...")
+		cd(Pkg.dir(madsmodule))
 		run(`git status -s`)
 		cd(cwd)
 	else
@@ -33,7 +33,7 @@ function status(testmod::AbstractString; git::Bool=true)
 		o = ""
 		try
 			(outRead, outWrite) = redirect_stdout();
-			Pkg.status(testmod)
+			Pkg.status(madsmodule)
 			o = readavailable(outRead);
 			close(outWrite);
 			close(outRead);
@@ -44,33 +44,33 @@ function status(testmod::AbstractString; git::Bool=true)
 		a = ascii(o)
 		print(a)
 		if ismatch(r"(dirty)", a)
-			warn("$testmod latest changes are not committed!")
+			warn("$madsmodule latest changes are not committed!")
 			tag_flag = false
 		elseif ismatch(r"[0-9]\+", a)
-			warn("$testmod latest changes are not tagged!")
+			warn("$madsmodule latest changes are not tagged!")
 			tag_flag = true
 		elseif ismatch(r"master", a)
-			info("$testmod latest changes are already tagged!")
+			info("$madsmodule latest changes are already tagged!")
 			tag_flag = false
 		end
 		return tag_flag
 	end
 end
 
-"Tag the Mads modules"
-function tag()
+"Tag the Mads modules with a default argument `:patch`"
+function tag(sym::Symbol=:patch)
 	for i in madsmodules
-		Mads.tag(i)
+		Mads.tag(i; sym=sym)
 	end
 end
 
-function tag(testmod::AbstractString)
-	tag_flag = Mads.status(testmod, git=false)
+function tag(madsmodule::AbstractString, sym::Symbol=:patch)
+	tag_flag = Mads.status(madsmodule, git=false)
 	if tag_flag
-		Pkg.tag(testmod)
-		info("$testmod is now tagged!")
+		Pkg.tag(madsmodule, sym)
+		info("$madsmodule is now tagged!")
 	else
-		warn("$testmod cannot be tagged!")
+		warn("$madsmodule cannot be tagged!")
 	end
 end
 
