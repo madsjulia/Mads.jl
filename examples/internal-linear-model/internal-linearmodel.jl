56ef58025ae25cc43016a00982b3a252464d7374
Daniel O'Malley <omalled@lanl.gov>
2016-08-11 21:48:45 -0600

    bugfix for scott

--- examples/internal-linear-model/internal-linearmodel.jl
+++ examples/internal-linear-model/internal-linearmodel.jl
@@ -1,6 +1,6 @@
 import DataStructures
 
-function madsmodelrun(parameters::Associative) # model run
+function madsmodelrun_internal_linearmodel(parameters::Associative) # model run
 	f(t) = parameters["a"] * t - parameters["b"] # a * t - b
 	times = 1:4
 	predictions = DataStructures.OrderedDict{AbstractString, Float64}(zip(map(i -> string("o", i), times), map(f, times)))
--- examples/internal-polynomial-model/internal-polynomial.jl
+++ examples/internal-polynomial-model/internal-polynomial.jl
@@ -1,7 +1,7 @@
 import Mads
 import DataStructures
 
-function makemadsmodelrun(madsdata::Associative)
+function makemadsmodelrun_internal_polynomial(madsdata::Associative)
 	times = Mads.getobstime(madsdata)
 	names = Mads.getobskeys(madsdata)
 	function madsmodelrun(parameters::Associative) # model run
