0f3f46553e76b83103e6cb126ff1eeae5a107df4
monty <velimir.vesselinov@gmail.com>
2016-09-04 11:20:54 -0600

    minor

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,6 @@
 import os
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
-	html_theme = 'material'
-	html_context = {                                                             
-	'css_files': [                                                           
-	'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
-	'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
-	'_static/theme_overrides.css',                                       
-	],                                                                       
-	}
+    html_theme = 'material'
 else:
-	html_theme = 'material'
+    html_theme = 'material'
