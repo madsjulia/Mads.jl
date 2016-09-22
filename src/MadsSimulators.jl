7a23def5038e02a0cbd0f74793b7b8c3df0aafca
monty <velimir.vesselinov@gmail.com>
2016-09-22 16:17:36 -0600

    runcmd modified

--- src/MadsSimulators.jl
+++ src/MadsSimulators.jl
@@ -1,15 +1,23 @@
 """
 Execute amanzi
 """
-function amanzi(filename::AbstractString, nproc::Int=nprocs_per_task, quiet::Bool=true, observation_filename::AbstractString="observations.out", setup::AbstractString="source-amanzi-setup")
+function amanzi(filename::AbstractString, nproc::Int=nprocs_per_task, quiet::Bool=true, observation_filename::AbstractString="observations.out", setup::AbstractString="source-amanzi-setup", amanzi_exe::AbstractString="")
 	if quiet
 		quiet_string = "&> /dev/null"
 	else
 		quiet_string = ""
 	end
-	if nproc > 1
-		runcmd(`bash -l -c "source $setup; rm -f $observation_filename; mpirun -n $nproc $$AMANZI_EXE --xml_file=$filename $quiet_string"`)
+	if amanzi_exe == ""
+		if nproc > 1
+			runcmd(`bash -l -c "source $setup; rm -f $observation_filename; mpirun -n $nproc $$AMANZI_EXE --xml_file=$filename $quiet_string"`)
+		else
+			runcmd(`bash -l -c "source $setup; rm -f $observation_filename; $$AMANZI_EXE --xml_file=$filename $quiet_string"`)
+		end
 	else
-		runcmd(`bash -l -c "source $setup; rm -f $observation_filename; $$AMANZI_EXE --xml_file=$filename $quiet_string"`)
+		if nproc > 1
+			runcmd(`bash -l -c "source $setup; rm -f $observation_filename; mpirun -n $nproc $amanzi_exe --xml_file=$filename $quiet_string"`)
+		else
+			runcmd(`bash -l -c "source $setup; rm -f $observation_filename; $amanzi_exe --xml_file=$filename $quiet_string"`)
+		end
 	end
 end
