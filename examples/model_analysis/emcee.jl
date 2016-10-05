d8929530ef03d6ba6fa5b78af1a8697e2ac2ddf4
monty <velimir.vesselinov@gmail.com>
2016-10-05 10:19:45 -0600

    minor

--- examples/model_analysis/emcee.jl
+++ examples/model_analysis/emcee.jl
@@ -6,7 +6,7 @@ if !isdir("emcee_results")
 	mkdir("emcee_results")
 end
 
-chain, llhoods = Mads.emcee(md; numwalkers=3*nworkers(), nsteps=10000, burnin=1000, thinning=100, seed=2016, sigma=0.01)
+chain, llhoods = Mads.emcee(md; numwalkers=nworkers()+1, nsteps=10000, burnin=1000, thinning=100, seed=2016, sigma=0.01)
 Mads.scatterplotsamples(md, chain', "emcee_results/emcee_scatter.png")
 o = Mads.forward(md, chain')
 Mads.spaghettiplot(md, o, filename="emcee_results/emcee_spaghetti.png")
