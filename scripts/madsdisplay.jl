209428988fe5406e4bc5fa67e60d73942cebf7b9
monty <velimir.vesselinov@gmail.com>
2016-10-16 10:57:14 -0600

    madsdisplay.jl script added

---
+++ scripts/madsdisplay.jl
@@ -0,0 +1,59 @@
+import Images
+
+function madsgetextension(filename)
+	d = split(filename, "/")
+	s = split(d[end], ".")
+	if length(s) > 1
+		return s[end]
+	else
+		return ""
+	end
+end
+
+function madsgetrootname(filename::AbstractString; first=true)
+	d = split(filename, "/")
+	s = split(d[end], ".")
+	if !first && length(s) > 1
+		r = join(s[1:end-1], ".")
+	else
+		r = s[1]
+	end
+	if length(d) > 1
+		r = join(d[1:end-1], "/") * "/" * r
+	end
+	return r
+end
+
+function madsdisplay(filename::AbstractString)
+	if isdefined(:TerminalExtensions)
+		trytoopen = false
+		ext = lowercase(getextension(filename))
+		if ext == "svg"
+			root = getrootname(filename)
+			filename2 = root * ".png"
+			try
+				run(`convert -density 90 -background none $filename $filename2`)
+				img = Images.load(filename2)
+				Base.display(img)
+			catch
+				trytoopen = true
+			end
+		else
+			img = Images.load(filename)
+			Base.display(img)
+		end
+	else
+		trytoopen = true
+	end
+	if trytoopen
+		try
+			run(`open $filename`)
+		catch
+			try
+				run(`xdg-open $filename`)
+			catch
+				warn("Do not know how to open $filename")
+			end
+		end
+	end
+end
