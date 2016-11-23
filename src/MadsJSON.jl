b1006a4e495fc9b61049adddc8fdb3c0fc7bf798
monty <velimir.vesselinov@gmail.com>
2016-11-23 16:44:58 -0700

    ASCIIString is needed; urgh

--- src/MadsJSON.jl
+++ src/MadsJSON.jl
@@ -6,7 +6,7 @@ function loadjsonfile(filename::String) # load JSON text file
 	sz = filesize(filename)
 	f = open(filename, "r")
 	a = Mmap.mmap(f, Vector{UInt8}, sz)
-	s = convert(String, a)
+	s = convert(ASCIIString, a) # ASCIIString is needed; urgh
 	data = JSON.parse(s; dicttype=DataStructures.OrderedDict)
 	finalize(a)
 	close(f)
