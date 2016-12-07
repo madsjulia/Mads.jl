c17cc8917baba302feb67873cc6d45aa7820653c
monty <velimir.vesselinov@gmail.com>
2016-12-07 15:50:27 -0700

    minor fix of model_analysis

--- examples/model_analysis/emcee.jl
+++ examples/model_analysis/emcee.jl
@@ -7,7 +7,7 @@ if !isdir("emcee_results")
 end
 
 Mads.setobsweights!(md, 10)
-chain, llhoods = Mads.emcee(md; numwalkers=100, nsteps=1000000, burnin=100000, thinning=100, seed=2016, sigma=0.01)
+chain, llhoods = Mads.emceesampling(md; numwalkers=100, nsteps=1000000, burnin=100000, thinning=100, seed=2016, sigma=0.01)
 Mads.scatterplotsamples(md, chain', "emcee_results/emcee_scatter.png")
 o = Mads.forward(md, chain')
 Mads.spaghettiplot(md, o, filename="emcee_results/emcee_spaghetti.png")
