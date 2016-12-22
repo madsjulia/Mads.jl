21936554bac5fff9d11e74bc8dca2a896819fdd5
monty <velimir.vesselinov@gmail.com>
2016-12-22 15:58:24 -0700

    parallel example modified

--- examples/embed_c/embed_c.jl
+++ examples/embed_c/embed_c.jl
@@ -1,17 +1,18 @@
-import Mads
 import Base.Test
 
-@everywhere fcsqrt(d) = ccall( (:my_c_sqrt, "libmy.dylib"), Float64, (Float64,), d )
-@everywhere fcfunc_ex1(n, d) = ccall( (:my_c_func_ex1, "libmy.dylib"), Float64, (Int64, Float64), n, d )
-@everywhere function fjfunc_ex1( n, d )
+rmprocs(1:nworkers())
+
+fcsqrt(d) = ccall( (:my_c_sqrt, "libmy.dylib"), Float64, (Float64,), d )
+fcfunc_ex1(n, d) = ccall( (:my_c_func_ex1, "libmy.dylib"), Float64, (Int64, Float64), n, d )
+function fjfunc_ex1( n, d )
 	r = 0
 	for i = 1:n
 		r += i / d
 	end
 	r
 end
-@everywhere fcfunc_ex2(n_x, x, n_o, o) = ccall( (:my_c_func_ex2, "libmy.dylib"), Int32, (Int64, Ptr{Float64}, Int64, Ptr{Float64}), n_x, x, n_o, o )
-@everywhere function fjfunc_ex2( x, o )
+fcfunc_ex2(n_x, x, n_o, o) = ccall( (:my_c_func_ex2, "libmy.dylib"), Int32, (Int64, Ptr{Float64}, Int64, Ptr{Float64}), n_x, x, n_o, o )
+function fjfunc_ex2( x, o )
 	n_o = length(o)
 	for j = 1:n_o
 		o[j] = 0
@@ -22,21 +23,25 @@ end
 		end
 	end
 end
-@everywhere fcmxv_bad(n_x, x, M, n_o, o) = ccall( (:my_c_mxv_bad, "libmy.dylib"), Int32, (Int64, Ptr{Float64}, Ptr{Float64}, Int64, Ptr{Float64}), n_x, x, M, n_o, o )
-@everywhere fcmxv(n_x, x, M, n_o, o) = ccall( (:my_c_mxv, "libmy.dylib"), Int32, (Int64, Ptr{Float64}, Ptr{Float64}, Int64, Ptr{Float64}), n_x, x, M, n_o, o )
-@everywhere fjmxv(M, x) = M .* x
+fcmxv_bad(n_x, x, M, n_o, o) = ccall( (:my_c_mxv_bad, "libmy.dylib"), Int32, (Int64, Ptr{Float64}, Ptr{Float64}, Int64, Ptr{Float64}), n_x, x, M, n_o, o )
+fcmxv(n_x, x, M, n_o, o) = ccall( (:my_c_mxv, "libmy.dylib"), Int32, (Int64, Ptr{Float64}, Ptr{Float64}, Int64, Ptr{Float64}), n_x, x, M, n_o, o )
+fjmxv(M, x) = M * x
 
 # sqrt comparison
-println("sqrt ...")
+info("sqrt ...")
+println("c ...")
 @time fcsqrt(2)
+println("julia ...")
 @time sqrt(2)
 @Base.Test.test_approx_eq fcsqrt(2) sqrt(2)
 
 # function example #1
-println("func #1 ...")
-@time fcfunc_ex1( 100, 6.4 )
-@time fjfunc_ex1( 100, 6.4 )
-@Base.Test.test_approx_eq fcfunc_ex1( 100, 6.4 ) fjfunc_ex1( 100, 6.4 )
+info("func #1 ...")
+println("c ...")
+@time fcfunc_ex1(100, 6.4)
+println("julia ...")
+@time fjfunc_ex1(100, 6.4)
+@Base.Test.test_approx_eq fcfunc_ex1(100, 6.4) fjfunc_ex1(100, 6.4)
 
 nP = 100
 nO = 1000000
@@ -45,36 +50,21 @@ o_c = Array(Float64, nO)
 o_j = Array(Float64, nO)
 
 # function example #2
-println("func #2 ...")
+info("func #2 ...")
+println("c ...")
 @time fcfunc_ex2(nP, x, nO, o_c)
+println("julia ...")
 @time fjfunc_ex2(x, o_j)
 @Base.Test.test_approx_eq maximum(abs(o_c - o_j)) 0
 
 M = ones(nO, nP)
 M[:,end] = 100000
 
-println("Matrix vector multiplication ...")
+info("Matrix vector multiplication ...")
+println("c (bad) ...")
 @time fcmxv_bad(nP, x, M, nO, o_c);
+println("c ...")
 @time fcmxv(nP, x, M, nO, o_c)
+println("julia ...")
 @time o_julia = fjmxv(M, x)
 @Base.Test.test_approx_eq maximum( o_c - o_julia ) 0
-
-Mads.setprocs(4)
-
-println("Matrix vector multiplication in parallel ...")
-println("C (bad) ...")
-@time fcmxv_bad(nP, x, M, nO, o_c);
-@time map(i->fcmxv_bad(nP, x, M, nO, o_c), 1:10);
-@time pmap(i->fcmxv_bad(nP, x, M, nO, o_c), 1:10);
-
-println("C ...")
-@time fcmxv(nP, x, M, nO, o_c);
-@time map(i->fcmxv(nP, x, M, nO, o_c), 1:10);
-@time pmap(i->fcmxv(nP, x, M, nO, o_c), 1:10);
-
-println("Julia ...")
-@time fjmxv(M, x);
-@time map(i->fjmxv(M, x), 1:10);
-@time pmap(i->fjmxv(M, x), 1:10);
-
-println("Done!")
--- examples/embed_c/embed_j.jl
+++ examples/embed_c/embed_j.jl
@@ -1,11 +1,13 @@
-import Mads
-import Base.Test
+@everywhere fjmxv(M, x) = M' .* x
 
-@everywhere fjmxv(M, x) = M .* x
+@everywhere nP = 100
+@everywhere nO = 10000
+
+@everywhere x = rand(nP)
+@everywhere M = ones(nO, nP)
+@everywhere M[:,end] = 100000
 
 println("Julia ...")
 @time fjmxv(M, x);
 @time map(i->fjmxv(M, x), 1:10);
 @time pmap(i->fjmxv(M, x), 1:10);
-
-println("Done!")
