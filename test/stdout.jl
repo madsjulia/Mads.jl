90a610fd189b4a20c01e801ff846d98ea80dccbc
monty <velimir.vesselinov@gmail.com>
2017-02-05 08:56:21 -0700

    stdout.jl improved

--- test/stdout.jl
+++ test/stdout.jl
@@ -1,9 +1,18 @@
 originalSTDOUT = STDOUT;
+originalSTDERR = STDERR;
 (outRead, outWrite) = redirect_stdout();
-reader = @async readstring(outRead);
+(errRead, errWrite) = redirect_stderr();
+outreader = @async readstring(outRead);
+errreader = @async readstring(errRead);
 println("test")
+println(STDERR, "Goodbye, World!")
 redirect_stdout(originalSTDOUT);
+redirect_stderr(originalSTDERR);
 close(outWrite);
 output = wait(reader);
 close(outRead);
+close(errWrite);
+error = wait(errreader);
+close(errRead);
 print(output)
+print(error)
