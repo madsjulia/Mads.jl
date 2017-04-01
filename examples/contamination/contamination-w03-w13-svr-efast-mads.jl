de51b5288e7939f65a633cbb047d15ec1b633584
monty <velimir.vesselinov@gmail.com>
2017-04-01 16:25:58 -0600

    MadsSVR fixes

--- examples/contamination/contamination-w03-w13-svr-efast-mads.jl
+++ examples/contamination/contamination-w03-w13-svr-efast-mads.jl
@@ -1,22 +1,22 @@
 import Mads
 import SVR
 
-md = Mads.loadmadsfile("w03.mads") # load Mads input file into Julia Dictionary
+md = Mads.loadmadsfile("w03.mads")
 rootname = Mads.getmadsrootname(md)
 
-Mads.allwellsoff!(md) # turn off all wells
-Mads.wellon!(md, "w13a") # use well w13a
+Mads.allwellsoff!(md)
+Mads.wellon!(md, "w13a")
 
-svrexec, svrclean = Mads.makesvrmodel(md, 1000)
+svrexec, svrread, svrsave, svrclean  = Mads.makesvrmodel(md, 1000)
 
 numberofsamples = 100
 paramdict = Mads.getparamrandom(md, numberofsamples)
 paramarray = hcat(map(i->collect(paramdict[i]), keys(paramdict))...)
 @time predictions = Mads.forward(md, paramdict)'
 
-Mads.madsinfo("Training set spaghetti plot ...")
-Mads.spaghettiplot(md, predictions, keyword="w13a-training-set", format="PNG")
-Mads.display("$rootname-w13a-training-set-$numberofsamples-spaghetti.png")
+Mads.madsinfo("Model predictions ...")
+Mads.spaghettiplot(md, predictions, keyword="w13a-model", format="PNG")
+Mads.display("$rootname-w13a-model-$numberofsamples-spaghetti.png")
 
 @time svrpredictions = svrexec(paramarray)
 info("SVR discrepancy $(maximum(abs.(svrpredictions .- predictions)))")
@@ -26,15 +26,18 @@ Mads.spaghettiplot(md, svrpredictions, keyword="w13a-svr", format="PNG")
 Mads.display("$rootname-w13a-svr-$numberofsamples-spaghetti.png")
 
 sa = Mads.efast(md)
-Mads.plotobsSAresults(md, sa)
-Mads.display("$rootname-efast-385.svg")
+Mads.plotobsSAresults(md, sa, format="PNG")
+Mads.display("$rootname-efast-385.png")
 
-mdsvr["Julia model"] = svrexec
 mdsvr = deepcopy(md)
+mdsvr["Julia model"] = svrexec
 mdsvr["Filename"] = "$rootname-svr.mads"
 
 sasvr = Mads.efast(mdsvr)
-Mads.plotobsSAresults(mdsvr, sasvr)
-Mads.display("$rootname-svr-efast-385.svg")
+Mads.plotobsSAresults(mdsvr, sasvr, format="PNG")
+Mads.display("$rootname-svr-efast-385.png")
 
-# svrclean()
+svrsave()
+svrclean()
+svrread()
+svrclean()
--- src/MadsSVR.jl
+++ src/MadsSVR.jl
@@ -31,7 +31,10 @@ function svrtrain(madsdata::Associative, numberofsamples::Integer=100; check::Bo
 			y_pr = SVR.predict(sm, paramarray');
 			svrpredictions2 = [svrpredictions2; y_pr']
 		end
-		savefile && SVR.savemodel(sm, "$rootname-$i.svr")
+		if savefile
+			Mads.mkdir("svrmodels")
+			SVR.savemodel(sm, joinpath("svrmodels", "$rootname-$i-$numberofsamples.svr"))
+		end
 	end
 	if check
 		Mads.spaghettiplot(madsdata, predictions, keyword="svr-training", format="PNG")
@@ -47,30 +50,19 @@ function svrtrain(madsdata::Associative, numberofsamples::Integer=100; check::Bo
 	return svrmodel
 end
 
-function svrpredict(svrmodel::Array{SVR.svmmodel, 1}, paramarray::Array{Float64, 1}; loadfile::Bool=false, rootname::String="")
+function svrpredict(svrmodel::Array{SVR.svmmodel, 1}, paramarray::Array{Float64, 1})
 	npred = length(svrmodel)
 	y = Array(Float64, npred)
 	for i=1:npred
-		if loadfile && rootname != ""
-			sm = SVR.loadmodel("$rootname-$i.svr")
-		else
-			sm = svrmodel[i]
-		end
-		y[i] = SVR.predict(sm, paramarray');
+		y[i] = SVR.predict(svrmodel[i], paramarray');
 	end
 	return y
 end
-
-function svrpredict(svrmodel::Array{SVR.svmmodel, 1}, paramarray::Array{Float64, 2}; loadfile::Bool=false, rootname::String="")
+function svrpredict(svrmodel::Array{SVR.svmmodel, 1}, paramarray::Array{Float64, 2})
 	npred = length(svrmodel)
 	y = Array(Float64, 0, size(paramarray, 1))
 	for i=1:npred
-		if loadfile && rootname != ""
-			sm = SVR.loadmodel("$rootname-$i.svr")
-		else
-			sm = svrmodel[i]
-		end
-		y = [y; SVR.predict(sm, paramarray')'];
+		y = [y; SVR.predict(svrmodel[i], paramarray')'];
 	end
 	return y
 end
@@ -97,29 +89,77 @@ function svrfree(svrmodel::Array{SVR.svmmodel, 1})
 end
 
 """
+Dump SVR models in files
+
+$(documentfunction(svrdump))
+"""
+function svrdump(svrmodel::Array{SVR.svmmodel, 1}, rootname::String, numberofsamples::Int)
+	npred = length(svrmodel)
+	Mads.mkdir("svrmodels")
+	for i=1:npred		
+		if isdefined(svrmodel, i)
+			SVR.savemodel(svrmodel[i], joinpath("svrmodels", "$rootname-$i-$numberofsamples.svr"))
+		end
+	end
+	nothing
+end
+
+"""
+Load SVR models from files
+
+$(documentfunction(svrload))
+"""
+function svrload(npred::Int, rootname::String, numberofsamples::Int)
+	svrmodel = Array(SVR.svmmodel, npred)
+	for i=1:npred
+		filename = joinpath("svrmodels", "$rootname-$i-$numberofsamples.svr")	
+		if isfile(filename)
+			svrmodel[i] = SVR.loadmodel(filename)
+		else
+			warn("$filename does not exist")
+		end
+	end
+	return svrmodel
+end
+
+"""
 Make SVR model functions (executor and cleaner)
 
 $(documentfunction(svrfree))
 """
-function makesvrmodel(madsdata::Associative, numberofsamples::Integer=100; check::Bool=false, addminmax::Bool=true, savefile::Bool=false, svm_type::Int32=SVR.EPSILON_SVR, kernel_type::Int32=SVR.RBF, degree::Integer=3, gamma::Float64=1/numberofsamples, coef0::Float64=0.0, C::Float64=20000.0, nu::Float64=0.5, p::Float64=0.001, cache_size::Float64=100.0, eps::Float64=0.001, shrinking::Bool=true, probability::Bool=false, verbose::Bool=false)
+function makesvrmodel(madsdata::Associative, numberofsamples::Integer=100; check::Bool=false, addminmax::Bool=true, loaddata::Bool=false, savefile::Bool=false, svm_type::Int32=SVR.EPSILON_SVR, kernel_type::Int32=SVR.RBF, degree::Integer=3, gamma::Float64=1/numberofsamples, coef0::Float64=0.0, C::Float64=20000.0, nu::Float64=0.5, p::Float64=0.001, cache_size::Float64=100.0, eps::Float64=0.001, shrinking::Bool=true, probability::Bool=false, verbose::Bool=false)
 	rootname = Mads.getmadsrootname(madsdata)
 	optnames = Mads.getoptparamkeys(madsdata)
 	obsnames = Mads.getobskeys(madsdata)
-	svrmodel = svrtrain(madsdata, numberofsamples; check=check, addminmax=addminmax, savefile=savefile, svm_type=svm_type, kernel_type=kernel_type, gamma=gamma, coef0=coef0, C=C, nu=nu, p=p, cache_size=cache_size, eps=eps, shrinking=shrinking, probability=probability);
-	function svrexec(paramarray::Union{Array{Float64, 1}, Array{Float64, 2}}; loadfile::Bool=false, rootname::String="")
-		return svrpredict(svrmodel, paramarray; loadfile=loadfile, rootname=rootname)
+	npreds = length(obsnames)
+	function svrexec(paramarray::Union{Array{Float64, 1}, Array{Float64, 2}})
+		return svrpredict(svrmodel, paramarray)
 	end
-	function svrexec(paramdict::Associative; loadfile::Bool=false, rootname::String="")
+	function svrexec(paramdict::Associative)
 		d = DataStructures.OrderedDict{String, Float64}()
 		for k in optnames
 			d[k] = paramdict[k]
 		end
 		parvector = collect(values(d))
-		p = svrpredict(svrmodel, parvector'; loadfile=loadfile, rootname=rootname)
+		p = svrpredict(svrmodel, parvector')
 		DataStructures.OrderedDict{String, Float64}(zip(obsnames, p))
 	end
 	function svrclean()
 		svrfree(svrmodel)
 	end
-	return svrexec, svrclean
+	function svrsave()
+		svrdump(svrmodel, rootname, numberofsamples)
+	end
+	function svrread()
+		svrmodel = svrload(npreds, rootname, numberofsamples)
+		nothing
+	end
+
+	if loaddata
+		svrread()
+	else
+		svrmodel = svrtrain(madsdata, numberofsamples; check=check, addminmax=addminmax, savefile=savefile, svm_type=svm_type, kernel_type=kernel_type, gamma=gamma, coef0=coef0, C=C, nu=nu, p=p, cache_size=cache_size, eps=eps, shrinking=shrinking, probability=probability);
+	end
+
+	return svrexec, svrread, svrsave, svrclean
 end
