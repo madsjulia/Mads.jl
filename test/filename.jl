64d27389695fd4bbc70d7e0f7f5bac1acbef3351
Daniel Livingston <daniel.livingston@live.com>
2017-05-10 13:05:19 -0600

    Mads unit test cleanup

--- test/filename.jl
+++ test/filename.jl
@@ -4,26 +4,28 @@ import Mads
 import Base.Test
 
 @Base.Test.testset "Filename" begin
-    test_dir = "filename_testing"
-    curdir = pwd()
-    Mads.rmdir(test_dir)
-    Mads.mkdir(test_dir)
-    
-    touch(joinpath(test_dir, "a-v01.mads"))
-    touch(joinpath(test_dir, "a-v02.mads"))
-    touch(joinpath(test_dir, "a-v03.mads"))
+	test_dir = "filename_testing"
+	jpath(file) = joinpath(test_dir, file)
+	
+	curdir = pwd()
+	Mads.rmdir(test_dir)
+	Mads.mkdir(test_dir)
+	
+	touch(jpath("a-v01.mads"))
+	touch(jpath("a-v02.mads"))
+	touch(jpath("a-v03.mads"))
 
-    # Verify that getnextmadsfilename returns the most recently modified file
-    @Base.Test.test Mads.getnextmadsfilename(joinpath(test_dir, "a-v01.mads")) == joinpath(test_dir, "a-v03.mads")
-    sleep(1)
-    touch(joinpath(test_dir, "a-v02.mads"))
-    @Base.Test.test Mads.getnextmadsfilename(joinpath(test_dir, "a-v01.mads")) == joinpath(test_dir, "a-v02.mads")
-    
-    # Verify that Mads parses filename prefixes and extensions
-    touch(joinpath(test_dir, "test.file.name.txt"))
-    @Base.Test.test Mads.getrootname(joinpath(test_dir, "test.file.name.txt")) == joinpath(test_dir, "test")
-    @Base.Test.test Mads.getextension(joinpath(test_dir, "a-v01.mads")) == "mads"
+	# Verify that getnextmadsfilename returns the most recently modified file
+	@Base.Test.test Mads.getnextmadsfilename(jpath("a-v01.mads")) == jpath("a-v03.mads")
+	sleep(1)
+	touch(jpath("a-v02.mads"))
+	@Base.Test.test Mads.getnextmadsfilename(jpath("a-v01.mads")) == jpath("a-v02.mads")
+	
+	# Verify that Mads parses filename prefixes and extensions
+	touch(jpath("test.file.name.txt"))
+	@Base.Test.test Mads.getrootname(jpath("test.file.name.txt")) == jpath("test")
+	@Base.Test.test Mads.getextension(jpath("a-v01.mads")) == "mads"
 
-    Mads.rmdir("filename_testing")
+	Mads.rmdir("filename_testing")
 
 end
