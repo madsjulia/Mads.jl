db280d2816bfe35797a9378a248299f60c3c16cc
Daniel Livingston <daniel.livingston@live.com>
2017-05-09 18:20:18 -0600

    Unit test: test/filename.jl updated

--- test/filename.jl
+++ test/filename.jl
@@ -1,13 +1,30 @@
-Mads.rmdir("finename_testing")
-curdir = pwd()
-mkdir("finename_testing")
-cd("finename_testing")
-touch("a-v01.mads")
-touch("a-v02.mads")
-touch("a-v03.mads")
-@Base.Test.test Mads.getnextmadsfilename("a-v01.mads") == joinpath(".", "a-v03.mads")
-sleep(1)
-touch("a-v02.mads")
-@Base.Test.test Mads.getnextmadsfilename("a-v01.mads") == joinpath(".", "a-v02.mads")
-cd(curdir)
-Mads.rmdir("finename_testing")
+# Test for the Mads IO system
+
+import Mads
+import Base.Test
+
+@Base.Test.testset "Filename" begin
+    test_dir = "filename_testing"
+    curdir = pwd()
+    Mads.rmdir(test_dir)
+    Mads.mkdir(test_dir)
+    
+    touch(joinpath(test_dir, "a-v01.mads"))
+    touch(joinpath(test_dir, "a-v02.mads"))
+    touch(joinpath(test_dir, "a-v03.mads"))
+
+    # Verify that getnextmadsfilename returns the most recently modified file
+    @Base.Test.test Mads.getnextmadsfilename(joinpath(test_dir, "a-v01.mads")) == joinpath(test_dir, "a-v03.mads")
+    sleep(1)
+    touch(joinpath(test_dir, "a-v02.mads"))
+    @Base.Test.test Mads.getnextmadsfilename(joinpath(test_dir, "a-v01.mads")) == joinpath(test_dir, "a-v02.mads")
+    
+    # Verify that Mads parses filename prefixes and extensions
+    touch(joinpath(test_dir, "test.file.name.txt"))
+    @Base.Test.test Mads.getrootname(joinpath(test_dir, "test.file.name.txt")) == joinpath(test_dir, "test")
+    @Base.Test.test Mads.getextension(joinpath(test_dir, "a-v01.mads")) == "mads"
+
+    Mads.rmdir("filename_testing")
+    println(Mads.getmadsinputfile())
+
+end
