51a28f7d16fee4486378c68900b6c258f47ac295
Daniel Livingston <daniel.livingston@live.com>
2017-05-10 19:16:16 -0600

    Unit tests: more revised core tests

--- test/observations.jl
+++ test/observations.jl
@@ -1,19 +1,40 @@
-Mads.isobs(Dict("a"=>1,"b"=>1), Dict("a"=>1))
-Mads.isobs(Dict("a"=>1), Dict("a"=>1))
-Mads.gettime(Dict("a"=>1))
-Mads.settime!(Dict("a"=>1), 4)
-Mads.settime!(Dict("a"=>1,"time"=>1), 4)
-Mads.settime!(Dict("a"=>1,"t"=>1), 4)
-Mads.setweight!(Dict("a"=>1), 4)
-Mads.setweight!(Dict("a"=>1,"weight"=>1), 4)
-Mads.setweight!(Dict("a"=>1,"w"=>15), 4)
-Mads.settarget!(Dict("a"=>1), 4)
-Mads.settarget!(Dict("a"=>1,"target"=>1), 4)
-Mads.settarget!(Dict("a"=>1,"c"=>1), 4)
-Mads.getobsdist(Dict("Observations"=>Dict("a"=>Dict())))
-Mads.getobsdist(Dict("Observations"=>Dict("a"=>Dict("log"=>true))))
-Mads.getobsdist(Dict("Observations"=>Dict("a"=>Dict())))
-Mads.getobsmin(Dict("Observations"=>Dict("a"=>Dict("dist"=>"Uniform(14,15)"))))
-Mads.getobsmax(Dict("Observations"=>Dict("a"=>Dict("dist"=>"Uniform(14,15)"))))
-Mads.getweight(Dict("w"=>1))
-Mads.getweight(Dict("a"=>1))
+import Mads
+import Base.Test
+
+# Test observations functions
+function test_obs()
+	test_dict = Dict("a"=>1)
+	@Base.Test.test Mads.isobs(test_dict, Dict("a"=>1)) == true
+	
+	test_dict["b"] = 2
+	@Base.Test.test Mads.isobs(test_dict, Dict("a"=>1)) == false
+	
+	obs_dict = Dict("Observations"=>Dict("a"=>Dict()))
+	@Base.Test.test Mads.getobsdist(obs_dict) == String["Uniform(-1e6, 1e6)"]
+	
+	obs_dict["Observations"]["a"] = Dict("log"=>true)
+	@Base.Test.test Mads.getobsdist(obs_dict) == String["Uniform(1e-6, 1e6)"]
+	
+	obs_dict = Dict("Observations"=>Dict("a"=>Dict("dist"=>"Uniform(14,15)")))
+	@Base.Test.test Mads.getobsmin(obs_dict) == [14]
+	@Base.Test.test Mads.getobsmax(obs_dict) == [15]
+end
+
+# Test parameter setting functions
+function test_params(a=4,b=12,c=7)
+	test_dict = Dict("a"=>1)
+	
+	Mads.settime!(test_dict, a)
+	Mads.setweight!(test_dict, b)
+	Mads.settarget!(test_dict, c)
+	
+	@Base.Test.test Mads.gettime(test_dict) == a
+	@Base.Test.test Mads.getweight(test_dict) == b
+	@Base.Test.test Mads.gettarget(test_dict) == c
+end
+
+
+@Base.Test.testset "Observations" begin
+	test_obs()
+	test_params()
+end
--- test/parameters.jl
+++ test/parameters.jl
@@ -1,22 +1,51 @@
-Mads.isparam(Dict("a"=>1,"b"=>1), Dict("a"=>1))
-Mads.isparam(Dict("a"=>1), Dict("a"=>1))
-Mads.getparamsmin(Dict("Parameters"=>Dict("a"=>Dict())))
-Mads.getparamsmin(Dict("Parameters"=>Dict("a"=>Dict("log"=>true))))
-Mads.getparamsmax(Dict("Parameters"=>Dict("a"=>Dict())))
-Mads.getparamsmax(Dict("Parameters"=>Dict("a"=>Dict("log"=>true))))
-Mads.getparamsinit_min(Dict("Parameters"=>Dict("a"=>Dict("log"=>true))))
-Mads.getparamsinit_min(Dict("Parameters"=>Dict("a"=>Dict("init_min"=>14))))
-Mads.getparamsinit_max(Dict("Parameters"=>Dict("a"=>Dict("log"=>true))))
-Mads.getparamsinit_max(Dict("Parameters"=>Dict("a"=>Dict("init_max"=>14))))
-Mads.getparamdistributions(Dict("Parameters"=>Dict("a"=>Dict("init_dist"=>"Uniform(14,15)"))), init_dist=true)
-Mads.getparamrandom(Dict("Parameters"=>Dict("a"=>Dict("log"=>true, "type"=>"opt", "dist"=>"Uniform(14,15)"))), "a")
-Mads.getparamrandom(Dict("Parameters"=>Dict("a"=>Dict("log"=>true, "type"=>"opt", "dist"=>"Normal(14,7)"))), "a")
-Mads.getparamrandom(Dict("Parameters"=>Dict("a"=>Dict("log"=>true, "type"=>"opt", "dist"=>"Normal(14,7)"))), "b")
-Mads.void2nan!(Dict("a"=>Dict{Any,Any}("a"=>nothing)))
-Mads.void2nan!(Dict("a"=>Dict{Any,Any}("a"=>Dict{Any,Any}("a"=>nothing))))
-Mads.filterkeys(Dict("a"=>1), "a")
-Mads.filterkeys(Dict("a"=>1), r"a")
-Mads.getdictvalues(Dict("a"=>1), "a")
-Mads.getdictvalues(Dict("a"=>1), r"a")
-Mads.maxtorealmax!(convert(DataFrames.DataFrame, [1:4 1:4]))
-Mads.maxtorealmax!(convert(DataFrames.DataFrame, [[1,2] [1,realmax(Float64)]]))
+import Mads
+import Base.Test
+
+# Test parameter functions
+function test_params()
+	test_dict = Dict("a"=>1)
+	@Base.Test.test Mads.isparam(test_dict, Dict("a"=>1)) == true
+	test_dict["b"] = 1
+	@Base.Test.test Mads.isparam(test_dict, Dict("a"=>1)) == false
+	
+	param_dict = Dict("Parameters"=>Dict("a"=>Dict()))
+	@Base.Test.test Mads.getparamsmin(param_dict) == [-1.0e6]
+	@Base.Test.test Mads.getparamsmax(param_dict) == [+1.0e6]
+	
+	param_dict["Parameters"]["a"] = Dict("log"=>true)
+	@Base.Test.test Mads.getparamsmin(param_dict) == [1.0e-6]
+	@Base.Test.test Mads.getparamsmax(param_dict) == [1.0e+6]
+	@Base.Test.test Mads.getparamsinit_min(param_dict) == [1.0e-6]
+	@Base.Test.test Mads.getparamsinit_max(param_dict) == [1.0e+6]
+	
+	param_dict["Parameters"]["a"] = Dict("init_min"=>14)
+	@Base.Test.test Mads.getparamsinit_min(param_dict) == [14]
+	param_dict["Parameters"]["a"] = Dict("init_max"=>16)
+	@Base.Test.test Mads.getparamsinit_max(param_dict) == [16]
+	
+	dist_dict = Dict("Parameters"=>Dict("a"=>Dict("log"=>true, "type"=>"opt", "dist"=>"Uniform(14,15)")))
+	@Base.Test.test isapprox(Mads.getparamrandom(dist_dict, "a")[1], 14.5, atol=0.5)
+	#dist_dict["Parameters"]["a"]["dist"] = "Normal(14,7)"
+	#println(Mads.getparamrandom(dist_dict, "a"))
+end
+
+# Test various dictionary filtering functions
+function test_misc()
+	@Base.Test.test Mads.void2nan!(Dict("a"=>Dict{Any,Any}("a"=>nothing))) == nothing
+	@Base.Test.test Mads.void2nan!(Dict("a"=>Dict{Any,Any}("a"=>Dict{Any,Any}("a"=>nothing)))) == nothing
+	
+	basic_dict = Dict("a"=>1)
+	@Base.Test.test Mads.filterkeys(basic_dict, "a") == String["a"]
+	@Base.Test.test Mads.filterkeys(basic_dict, r"a") == String["a"]
+	@Base.Test.test Mads.getdictvalues(basic_dict, "a")[1][2] == 1
+	@Base.Test.test Mads.getdictvalues(basic_dict, r"a")[1][2] == 1
+	
+	# TODO: Verify that maxtorealmax! is working correctly...returns `nothing` currently
+	#@Base.Test.test 4 == Mads.maxtorealmax!(convert(DataFrames.DataFrame, [1:4 1:4]))
+	#@Base.Test.test realmax(Float32) == Mads.maxtorealmax!(convert(DataFrames.DataFrame, [[1,2] [1,realmax(Float64)]]))
+end
+
+@Base.Test.testset "asd" begin
+	test_params()
+	test_misc()
+end
