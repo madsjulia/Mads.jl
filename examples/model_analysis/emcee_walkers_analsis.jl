fb97512a53fb1dab17032c0d40247ec2514fcb44
monty <velimir.vesselinov@gmail.com>
2017-06-15 17:23:33 -0600

    /emcee_walkers_analsis.jl added

---
+++ examples/model_analysis/emcee_walkers_analsis.jl
@@ -0,0 +1,17 @@
+import Mads
+import Gadfly
+md = Mads.loadmadsfile(joinpath("models", "internal-polynomial.mads"))
+
+Mads.mkdir("emcee_results")
+
+info("AffineInvariantMCMC (EMCEE) Bayesian analysis with different number of walkers:")
+for nw = (50, 20, 10, 5, 1)
+	Mads.setobsweights!(md, 1000000)
+	chain, llhoods  = Mads.emceesampling(md; numwalkers=nw, nsteps=1000000, burnin=100000, thinning=100, seed=2016)
+	Mads.scatterplotsamples(md, chain', "emcee_results/emcee_init_nw$nw.png")
+	o = Mads.forward(md, chain')
+	Mads.spaghettiplot(md, o, filename="emcee_results/emcee_init_nw$(nw)_spaghetti.png")
+	@printf "Init: Number of walkers %d ->`o5` prediction: min = %f max = %f\n" nw min(o[:,5]...) max(o[:,5]...)
+	f = Gadfly.plot(x=o[:,5], Gadfly.Guide.xlabel("o5"), Gadfly.Geom.histogram())
+	Gadfly.draw(Gadfly.PNG("emcee_results/emcee_init_nw$(nw)_o5.png", 6Gadfly.inch, 4Gadfly.inch), f)
+end
