e4e2ddc249f24f00788892b3f22d6768cad63fc4
monty <velimir.vesselinov@gmail.com>
2017-06-22 17:54:19 -0600

    jump works global

--- examples/learn_julia/jump.jl
+++ examples/learn_julia/jump.jl
@@ -1,15 +1,25 @@
 import JuMP
 import Ipopt
 
-myf(a...) = sum(collect(a).^2)
+function testme()
+	global x
+	global function myf(a...)
+		sum(collect(a).^2)
+	end
 
-m = JuMP.Model(solver=Ipopt.IpoptSolver())
+	global m = JuMP.Model(solver=Ipopt.IpoptSolver())
 
-JuMP.register(m, :myf, 3, myf, autodiff=true)
-@JuMP.variable(m, x[1:3] >= 0.5)
-# @JuMP.NLobjective(m, Min, myf(x[1], x[2], x[3])) # works
-# @JuMP.NLobjective(m, Min, myf(x[1:3]...)) # does not work
-@eval @JuMP.NLobjective(m, Min, $(Expr(:call, :myf, [Expr(:ref, :x, i) for i=1:3]...))) # works
+	global nvar = 12
 
+	JuMP.register(m, :myf, nvar, myf, autodiff=true)
+	@JuMP.variable(m, x[1:nvar], start=rand())
 
-JuMP.solve(m)
+	# @JuMP.NLobjective(m, Min, myf(x[1], x[2], x[3])) # works
+	# @JuMP.NLobjective(m, Min, :("myf(x[1], x[2], x[3])")) # does not work
+	# @JuMP.NLobjective(m, Min, myf(x[1:3]...)) # does not work
+	@eval @JuMP.NLobjective(m, Min, $(Expr(:call, :myf, [Expr(:ref, :x, i) for i=1:nvar]...))) # works
+
+	JuMP.solve(m)
+end
+
+testme()
