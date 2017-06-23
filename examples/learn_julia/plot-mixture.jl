eab0298d26b0a5ce46bac64b0707fef4e2e78b4b
monty <velimir.vesselinov@gmail.com>
2017-06-23 17:04:31 -0600

    fixes

--- examples/learn_julia/plot-mixture.jl
+++ examples/learn_julia/plot-mixture.jl
@@ -2,24 +2,21 @@ import DataFrames
 import Gadfly
 import rMF
 
-cwd = pwd()
-
-nsource = 15
-nw = 100
-nc = 30
+nsource = 3
+nwell = 7
+ncomponent = 5
 sourcevec = map(i->"S$i", 1:nsource)
 
+cwd = pwd()
 cd(joinpath(rMF.rmfdir, "AquiferMixing"))
-rMF.loaddata("test"; nw=nw, nc=nc, ns=nsource, seed=2017)
+rMF.loaddata("test"; nw=nwell, nc=ncomponent, ns=nsource, seed=2017)
 rMF.execute(nsource)
 cd(cwd)
 
 observations = DataFrames.DataFrame()
 
-nw = length(rMF.uniquewells)
-ns = length(rMF.uniquespecies)
-observations[:Well] = repeat(rMF.uniquewells, outer=ns)
-observations[:Species] = repeat(rMF.uniquespecies, inner=nw)
+observations[:Well] = repeat(rMF.uniquewells, outer=ncomponent)
+observations[:Species] = repeat(rMF.uniquespecies, inner=nwell)
 observations[:Concentration] = [rMF.datamatrix...]
 
 p = Gadfly.plot(observations, xgroup="Well", x="Species", y="Concentration", color="Species", Gadfly.Geom.subplot_grid(Gadfly.Geom.bar()))
@@ -28,12 +25,12 @@ Gadfly.draw(Gadfly.PNG("observations.png", 10Gadfly.inch, 4Gadfly.inch, dpi=300)
 
 p = Gadfly.spy(rMF.datamatrix, Gadfly.Scale.x_discrete(labels = i->rMF.uniquespecies[i]), Gadfly.Scale.y_discrete(labels = i->rMF.uniquewells[i]), Gadfly.Guide.YLabel("Wells"), Gadfly.Guide.XLabel("Species"), Gadfly.Scale.ContinuousColorScale(Gadfly.Scale.lab_gradient(parse(Colors.Colorant, "green"), parse(Colors.Colorant, "yellow"), parse(Colors.Colorant, "red")), minvalue = 0, maxvalue = 1), Gadfly.Theme(key_position = :none))
 Mads.display(p)
-Gadfly.draw(Gadfly.PNG("observations-matrix.png", 1Gadfly.inch * ns, 1Gadfly.inch * nw, dpi=300), p)
+Gadfly.draw(Gadfly.PNG("observations-matrix.png", 1.5Gadfly.inch + 0.1Gadfly.inch * ncomponent, 1.5Gadfly.inch + 0.1Gadfly.inch * nwell, dpi=300), p)
 
 mixer = DataFrames.DataFrame()
 
 mixer[:Well] = repeat(rMF.uniquewells, outer=nsource)
-mixer[:Sources] = repeat(sourcevec, inner=nw)
+mixer[:Sources] = repeat(sourcevec, inner=nwell)
 mixer[:Ratio] = [rMF.mixers[nsource]...]
 
 p = Gadfly.plot(mixer, xgroup="Well", x="Sources", y="Ratio", color="Sources", Gadfly.Geom.subplot_grid(Gadfly.Geom.bar(position=:stack)), Gadfly.Scale.color_discrete_manual("blue","red","green"))
@@ -42,11 +39,11 @@ Gadfly.draw(Gadfly.PNG("mixer.png", 7Gadfly.inch, 4Gadfly.inch, dpi=300), p)
 
 p = Gadfly.spy(rMF.mixers[nsource], Gadfly.Scale.x_discrete(labels = i->i), Gadfly.Scale.y_discrete(labels = i->rMF.uniquewells[i]), Gadfly.Guide.YLabel("Wells"), Gadfly.Guide.XLabel("Sources"), Gadfly.Scale.ContinuousColorScale(Gadfly.Scale.lab_gradient(parse(Colors.Colorant, "green"), parse(Colors.Colorant, "yellow"), parse(Colors.Colorant, "red")), minvalue = 0, maxvalue = 1), Gadfly.Theme(key_position = :none))
 Mads.display(p)
-Gadfly.draw(Gadfly.PNG("mixer-matrix.png", 1Gadfly.inch * nsource, 1Gadfly.inch * nw, dpi=300), p)
+Gadfly.draw(Gadfly.PNG("mixer-matrix.png", 1.5Gadfly.inch + 0.1Gadfly.inch * nsource, 1.5Gadfly.inch + 0.1Gadfly.inch * nwell, dpi=300), p)
 
 source = DataFrames.DataFrame()
 
-source[:Sources] = repeat(sourcevec, outer=ns)
+source[:Sources] = repeat(sourcevec, outer=ncomponent)
 source[:Species] = repeat(rMF.uniquespecies, inner=nsource)
 source[:Concentration] = [rMF.buckets[nsource]...]
 
@@ -58,4 +55,4 @@ bm = rMF.buckets[nsource]
 bm[bm.>1] = 1
 p = Gadfly.spy(bm, Gadfly.Scale.x_discrete(labels = i->rMF.uniquespecies[i]), Gadfly.Scale.y_discrete(labels = i->i), Gadfly.Guide.YLabel("Sources"), Gadfly.Guide.XLabel("Species"), Gadfly.Scale.ContinuousColorScale(Gadfly.Scale.lab_gradient(parse(Colors.Colorant, "green"), parse(Colors.Colorant, "yellow"), parse(Colors.Colorant, "red")), minvalue = 0, maxvalue = 1), Gadfly.Theme(key_position = :none))
 Mads.display(p)
-Gadfly.draw(Gadfly.PNG("source-matrix.png", 1Gadfly.inch * ns, 1Gadfly.inch * nsource, dpi=300), p)
+Gadfly.draw(Gadfly.PNG("source-matrix.png", 1.5Gadfly.inch + 0.1Gadfly.inch * ncomponent, 1.5Gadfly.inch + 0.1Gadfly.inch * nsource, dpi=300), p)
