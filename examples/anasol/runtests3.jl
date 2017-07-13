2b348784049e976d7bfae22a3a370efcc14c2553
monty <velimir.vesselinov@gmail.com>
2017-07-13 07:47:54 -0600

    Mads (with dictionaries) is as fast as FastMadsAnasol

--- examples/anasol/runtests3.jl
+++ examples/anasol/runtests3.jl
@@ -6,24 +6,14 @@ srand(2017)
 
 workdir = joinpath(Mads.madsdir, "..", "examples", "anasol")
 md = Mads.loadmadsfile(joinpath(workdir, "w01purebig.mads"))
-ns = 10
+ns = 100
 rsetdict = Mads.getparamrandom(md, ns)
 rsetarray = hcat(map(i->rsetdict[i], keys(rsetdict))...)'
-rsetarrayplus = [repmat([823], ns)'; repmat([1499], ns)'; repmat([3], ns)'; rsetarray; repmat([15], ns)']
-Mads.vectoron()
-computeconcentrations = Mads.makedoublearrayfunction(md)
-@time rv = computeconcentrations(rsetarrayplus);
-# this is slow because concentration function is created each time
-#rfs = Array{Float64}(ns);
-#@time for i = 1:ns
-#    rfs[i] = Mads.forward(md, rsetarrayplus[:, i])
-#end
 
-Mads.vectoroff()
+info("Mads")
 @time rf = Mads.forward(md, rsetarray);
 
-# rv == rf
-
+info("FastMadsAnasol")
 include("/Users/monty/Julia/FastMadsAnasol.jl/base.jl")
 @makemadslikeanasol madslike "w01purebig.mads"
 ra = Array{Float64}(ns);
