f6d5214070a1bde9f2088047be792fceda14009e
monty <velimir.vesselinov@gmail.com>
2017-07-16 12:53:11 -0600

    add restart test script which might fail under v0.6

---
+++ examples/restart/runtests-short.jl
@@ -0,0 +1,6 @@
+workdir = joinpath(Mads.madsdir, "..", "examples", "restart")
+Mads.rmdir(joinpath(workdir, "w01_restart"))
+md = Mads.loadmadsfile(joinpath(workdir, "w01-v01.mads"))
+md["Restart"] = true
+create_restart_results = Mads.calibrate(md, np_lambda=1, maxEval=10, maxJacobians=2)
+
