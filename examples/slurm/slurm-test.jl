ab0720daf84757b6f8deee7358b4d8661551c7ad
monty <velimir.vesselinov@gmail.com>
2017-09-16 16:42:29 -0600

    slurm-test.jl modified

--- examples/slurm/slurm-test.jl
+++ examples/slurm/slurm-test.jl
@@ -15,26 +15,15 @@ info("$filename")
 md = Mads.loadmadsfile(filename)
 # Mads.addkeyword!(md, "respect_space")
 md["Restart"]= true
-# info("forward")
-# Mads.forward(md)
-#@show ReusableFunctions.restarts
-# info("local SA")
-# sa_results = Mads.localsa(md, datafiles=true, imagefiles=true)
-#@show ReusableFunctions.restarts
-#JLD.save("lsa_results.jld", "lsa_results", lsa_results)
-parameters_old = Mads.getparamdict(md)
-while true
-	info("calibrate")
-	inverse_parameters, inverse_results = Mads.calibrate(md; np_lambda=36, lambda_mu=1.5, tolX=1e-16)
-	if parameters_old == inverse_parameters
-		warn("Calibration done!")
-		break
-	end
-	parameters_old = inverse_parameters
-	#@show ReusableFunctions.restarts
-	Mads.setparamsinit!(md, inverse_parameters)
-	Mads.savemadsfile(md)
-	filename = Mads.setnewmadsfilename(md)
-	info("$filename")
-	md["Filename"] = filename
-end
+info("forward")
+Mads.forward(md)
+@show ReusableFunctions.restarts
+info("local SA")
+lsa_results = Mads.localsa(md, datafiles=true, imagefiles=true)
+@show ReusableFunctions.restarts
+JLD.save("lsa_results.jld", "lsa_results", lsa_results)
+info("calibrate")
+inverse_parameters, inverse_results = Mads.calibrate(md; np_lambda=36, lambda_mu=1.5, tolX=1e-16)
+@show ReusableFunctions.restarts
+Mads.setparamsinit!(md, inverse_parameters)
+Mads.savemadsfile(md)
