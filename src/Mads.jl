61873cbe90234bcd60f781182bd84d670e6649f3
monty <velimir.vesselinov@gmail.com>
2018-06-27 10:34:59 +0200

    minor v0.7 changes

--- src/Mads.jl
+++ src/Mads.jl
@@ -10,33 +10,32 @@ Licensing: GPLv3: http://www.gnu.org/licenses/gpl-3.0.html
 """
 module Mads
 
-madsgit = true
+global madsgit = true
 try
 	run(pipeline(`git help`, stdout=DevNull, stderr=DevNull))
 catch
-	madsgit = false
+	global madsgit = false
 end
 
-madsbash = true
-if !is_windows()
+global madsbash = true
+if !Sys.is_windows()
 	try
 		run(pipeline(`bash --help`, stdout=DevNull, stderr=DevNull))
 	catch
-		madsbash = false
+		global madsbash = false
 	end
 end
 
 """
 Mads Modules: $madsmodules
 """
-madsmodules = ["Mads", "Anasol", "AffineInvariantMCMC", "GeostatInversion", "Kriging", "BIGUQ", "ReusableFunctions", "RobustPmap", "MetaProgTools", "SVR", "DocumentFunction"]
+global madsmodules = ["Mads", "Anasol", "AffineInvariantMCMC", "GeostatInversion", "Kriging", "BIGUQ", "ReusableFunctions", "RobustPmap", "MetaProgTools", "SVR", "DocumentFunction"]
 
 import DocumentFunction
 import GeostatInversion
 import SVR
 
 include("MadsHelpers.jl")
-include("MadsCapture.jl")
 
 "Try to import a module"
 macro tryimport(s::Symbol)
@@ -88,65 +87,38 @@ if !haskey(ENV, "MADS_NO_PYTHON")
 	end
 end
 
-vectorflag = false
-quiet = true
-restart = false
-graphoutput = true
-graphbackend = "SVG"
-dpi=300
-verbositylevel = 1
-debuglevel = 1
-modelruns = 0
-madsinputfile = ""
-executionwaittime = 0.0
-sindxdefault = 0.1
-create_tests = false # dangerous if true
-long_tests = false # execute long tests
-madsservers = ["madsmax", "madsmen", "madsdam", "madszem", "madskil", "madsart", "madsend"]
-madsservers2 = ["madsmin"; map(i->(@sprintf "mads%02d" i), 1:18); "es05"; "es06"]
-nprocs_per_task_default = 1
+global vectorflag = false
+global quiet = true
+global restart = false
+global graphoutput = true
+global graphbackend = "SVG"
+global dpi=300
+global verbositylevel = 1
+global debuglevel = 1
+global modelruns = 0
+global madsinputfile = ""
+global executionwaittime = 0.0
+global sindxdefault = 0.1
+global create_tests = false # dangerous if true
+global long_tests = false # execute long tests
+global madsservers = ["madsmax", "madsmen", "madsdam", "madszem", "madskil", "madsart", "madsend"]
+global madsservers2 = ["madsmin"; map(i->(@sprintf "mads%02d" i), 1:18); "es05"; "es06"]
+global nprocs_per_task_default = 1
 const madsdir = splitdir(Base.source_path())[1]
 
 if haskey(ENV, "MADS_LONG_TESTS")
-	long_tests = true
+	global long_tests = true
 end
 
 if haskey(ENV, "MADS_QUIET")
-	quiet = true
+	global quiet = true
 end
 
 if haskey(ENV, "MADS_NOT_QUIET")
-	quiet = false
+	global quiet = false
 end
 
-if !haskey(ENV, "MADS_NO_PLOT")
-	if !haskey(ENV, "MADS_NO_PYTHON") && !haskey(ENV, "MADS_NO_PYPLOT")
-		@Mads.tryimport PyCall
-		@Mads.tryimport PyPlot
-		if !isdefined(:PyPlot)
-			ENV["MADS_NO_PYPLOT"] = ""
-		end
-	end
-	if !haskey(ENV, "MADS_NO_GADFLY")
-		@Mads.tryimport Gadfly
-		if !isdefined(:Gadfly)
-			ENV["MADS_NO_GADFLY"] = ""
-		end
-	end
-else
-	ENV["MADS_NO_GADFLY"] = ""
-	ENV["MADS_NO_PYPLOT"] = ""
-	ENV["MADS_NO_DISPLAY"] = ""
-	graphoutput = false
-	warn("Mads plotting is disabled")
-end
-
-if Mads.pkgversion("Gadfly") == v"0.6.1"
-	ENV["MADS_NO_GADFLY"] = ""
-	warn("Gadfly v0.6.1 has bugs; update or downgrade to another version!")
-	warn("Gadfly plotting is disabled!")
-end
-
+include("MadsCapture.jl")
 include("MadsLog.jl")
 include("MadsHelp.jl")
 include("MadsCreate.jl")
@@ -172,8 +144,36 @@ include("MadsAnasol.jl")
 include("MadsTestFunctions.jl")
 include("MadsSVR.jl")
 
+if Mads.pkgversion("Gadfly") == v"0.6.1"
+	ENV["MADS_NO_GADFLY"] = ""
+	warn("Gadfly v0.6.1 has bugs; update or downgrade to another version!")
+	warn("Gadfly plotting is disabled!")
+end
+
+if !haskey(ENV, "MADS_NO_PLOT")
+	if !haskey(ENV, "MADS_NO_GADFLY")
+		@Mads.tryimport Gadfly
+		if !isdefined(:Gadfly)
+			ENV["MADS_NO_GADFLY"] = ""
+		end
+	end
+	if !haskey(ENV, "MADS_NO_PYTHON") && !haskey(ENV, "MADS_NO_PYPLOT")
+		@Mads.tryimport PyCall
+		@Mads.tryimport PyPlot
+		if !isdefined(:PyPlot)
+			ENV["MADS_NO_PYPLOT"] = ""
+		end
+	end
+else
+	ENV["MADS_NO_GADFLY"] = ""
+	ENV["MADS_NO_PYPLOT"] = ""
+	ENV["MADS_NO_DISPLAY"] = ""
+	global graphoutput = false
+	warn("Mads plotting is disabled")
+end
+
 if haskey(ENV, "MADS_TRAVIS")
-	graphoutput = false
+	global graphoutput = false
 else
 	include(joinpath("..", "src-interactive", "MadsPublish.jl"))
 	include(joinpath("..", "src-interactive", "MadsParallel.jl"))
@@ -192,9 +192,7 @@ else
 	end
 end
 
-if !haskey(ENV, "MADS_NO_PYTHON") && !haskey(ENV, "MADS_NO_PYPLOT")
-	include("MadsPlotPy.jl")
-end
+include("MadsSenstivityAnalysis.jl")
 
 if !haskey(ENV, "MADS_NO_GADFLY")
 	include("MadsAnasolPlot.jl")
@@ -202,6 +200,8 @@ if !haskey(ENV, "MADS_NO_GADFLY")
 	include("MadsPlot.jl")
 end
 
-include("MadsSenstivityAnalysis.jl")
+if !haskey(ENV, "MADS_NO_PYTHON") && !haskey(ENV, "MADS_NO_PYPLOT")
+	include("MadsPlotPy.jl")
+end
 
 end
