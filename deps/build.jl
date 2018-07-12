4e3d7478f6abfaf53ade6b5609478fdee11e3fd3
monty <velimir.vesselinov@gmail.com>
2018-07-12 07:54:15 -0600

    deps/build.jl fixed

--- deps/build.jl
+++ deps/build.jl
@@ -8,10 +8,11 @@ else
 	const PACKAGES = ["pyyaml", "matplotlib"]
 
 	try
-		info("Installing python pip using PyCall ...")
+		info("Checking for python pip using PyCall ...")
 		eval(:(@PyCall.pyimport pip))
 	catch e
-		print(e.msg)
+		println(e)
+		warn("Python pip is not installed!")
 		info("Downloading & installing python pip ...")
 		get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
 		download("https://bootstrap.pypa.io/get-pip.py", get_pip)
@@ -19,35 +20,41 @@ else
 		rm("$get_pip")
 	end
 
-	eval(:(@PyCall.pyimport pip))
-	args = String[]
-	if haskey(ENV, "http_proxy")
-		push!(args, "--proxy")
-		push!(args, ENV["http_proxy"])
+	try
+		info("Installing Python YAML & MatPlotLib using pip ...")
+		eval(:(@PyCall.pyimport pip))
+		args = String[]
+		if haskey(ENV, "http_proxy")
+			push!(args, "--proxy")
+			push!(args, ENV["http_proxy"])
+		end
+		push!(args, "install")
+		push!(args, "--user")
+		append!(args, PACKAGES)
+		pip.main(args)
+	catch e
+		println(e)
+		warn("Installing Python YAML & MatPlotLib using pip fails!")
 	end
-	push!(args, "install")
-	push!(args, "--user")
-	append!(args, PACKAGES)
-
-	info("Installing Python YAML & MatPlotLib using pip ...")
-	pip.main(args)
 
 	try
 		eval(:(@PyCall.pyimport yaml))
 		info("Python pip YAML (pyyaml) is installed!")
 	catch e
-		print(e.msg)
-		warn("Python pip YAML (pyyaml) installation has failed! Using Conda instead ...")
+		println(e)
+		warn("Python pip YAML (pyyaml) installation has failed!")
+		info("Using Conda instead ...")
 		import Conda
-		Conda.add("pyyaml")
+		Conda.add("yaml")
 	end
 
 	try
 		eval(:(@PyCall.pyimport matplotlib))
 		info("Python pip MatPlotLib is installed!")
 	catch e
-		print(e.msg)
-		warn("Python pip MatPlotLib installation has failed! Using Conda instead ...")
+		println(e)
+		warn("Python pip MatPlotLib installation has failed!")
+		info("Using Conda instead ...")
 		import Conda
 		Conda.add("matplotlib")
 	end
