48937782560ae82f6c0094bd9846985cdddf18b4
monty <velimir.vesselinov@gmail.com>
2018-09-02 12:42:14 -0600

    w01-tensor.mads added

--- examples/tensor/tensor-time.jl
+++ examples/tensor/tensor-time.jl
@@ -2,14 +2,13 @@ import Mads
 md = Mads.loadmadsfile("w01-w13a_w20a-opt-tensor.mads")
 s = size(Mads.forwardgrid(md)[:,:,1])
 nstep = 10
-T = Array{Float64}(s[1],s[2],nstep)
+T = Array{Float64}(s[2],s[1],nstep)
 srand(2017)
 for i = 1:nstep
 	md["Grid"]["time"] = i * 10
 	g = Mads.forwardgrid(md)[:,:,1]
 	isnan(g) = minimum(g)
 	info("Time: $(md["Grid"]["time"]) Max conc $(maximum(g)) Min conc $(minimum(g))")
-	T[:,:,i] = g
-	T[:,:,i] = g
+	T[:,:,i] = g'
 end
 JLD.save("tensor.jld", "T", T)
