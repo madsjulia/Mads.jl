25f1726c44cf3f0db40e4e4856fcf77614d15d6b
monty <velimir.vesselinov@gmail.com>
2018-09-07 11:28:24 -0600

    tensor done

--- examples/tensor/tensor-time2noise.jl
+++ examples/tensor/tensor-time2noise.jl
@@ -5,7 +5,6 @@ import JLD
 # import rMF
 # cd(c)
 
-srand(1)
 md = Mads.loadmadsfile("w01-tensor2.mads")
 Mads.addsource!(md; dict=Dict("t0"=>200., "x"=>1100., "y"=>1450.))
 Mads.addsource!(md; dict=Dict("t0"=>400., "x"=>1200., "y"=>1550.))
@@ -56,7 +55,6 @@ nt, nw = size(V[1])
 wellnames = ntuple(i->"W$i", nw)
 C = reshape(hcat(V...), (nt,nw,3));
 W = C ./ maximum(sum(C, 3)) .* 0.9;
-W = W .+ ((randn(size(W)) / 40) .* W)
 
 # rMF.loaddata("test", ns=3, nc=4, nw=9, nt=101, seed=5)
 # Ht = deepcopy(rMF.truebucket)
@@ -77,17 +75,21 @@ for t = 1:nt
 		X[w,:,t] += (1-sum(W[t,w,:])) .* Hb
 	end
 end
-NTFk.plot2d(permutedims(Wt, (1,3,2)), permutedims(Wt, (1,3,2)); xtitle="", ytitle="", wellnames=wellnames, keyword="noise-input", xmax=100, ymax=1., gm=[Gadfly.Guide.manual_color_key("Sources", ["S1", "S2", "S3", "Background"], NTFk.colors[1:4]), Gadfly.Theme(major_label_font_size=16Gadfly.pt, key_label_font_size=14Gadfly.pt, minor_label_font_size=12Gadfly.pt)])
 
-Mads.plotseries(X[:,1,:]')
-Mads.plotseries(X[:,2,:]')
-Mads.plotseries(X[:,3,:]')
-Mads.plotseries(X[:,4,:]')
+srand(1)
+# Xn = X .+ ((randn(size(X)) / 80) .* X)
+Xn = convert(Array{Float32,3}, X .+ (randn(size(X)) * 10))
+Xn[Xn.<0] .= 0
+
+Mads.plotseries(Xn[:,1,:]')
+Mads.plotseries(Xn[:,2,:]')
+Mads.plotseries(Xn[:,3,:]')
+Mads.plotseries(Xn[:,4,:]')
 
 if isfile("ntfk-contamination-noise.jld")
 	Wem, Hem = JLD.load("nmfk-contamination-noise.jld", "We", "He")
 else
-	Wem, Hem, of, rob, aic = NMFk.execute(X[:,:,end], 2:5, 10; quiet=false, mixture=:mixmatch)
+	Wem, Hem, of, rob, aic = NMFk.execute(Xn[:,:,end], 2:5, 10; quiet=false, mixture=:mixmatch)
 	JLD.save("nmfk-contamination-noise.jld", "We", Wem, "He", Hem, "of", of, "rob", rob, "aic", aic)
 end
 display(X[:,:,end])
@@ -96,16 +98,16 @@ display(Wem[4] * Hem[4])
 if isfile("ntfk-contamination-noise.jld")
 	Wet, Het = JLD.load("ntfk-contamination-noise.jld", "We", "He")
 else
-	Wet, Het, of, rob, aic = NMFk.execute(X, 3:5, 10; maxouteriters=1000, tol=1e-3, tolX=1e-3, tolOF=1., quiet=false)
+	Wet, Het, of, rob, aic = NMFk.execute(Xn, 3:5, 10; maxouteriters=1000, tol=1e-3, tolX=1e-3, tolOF=1., quiet=false)
 	JLD.save("ntfk-contamination-noise.jld", "We", Wet, "He", Het, "of", of, "rob", rob, "aic", aic)
 end
 for i=3:5
-	Xe = NMFk.mixmatchcompute(X, Wet[i], Het[i])
-	info("Norm($i): $(vecnorm(Xe .- X))")
+	Xe = NMFk.mixmatchcompute(Xn, Wet[i], Het[i])
+	info("Norm($i): $(vecnorm(Xe .- Xn))")
 end
 NTFk.plot2d(permutedims(Wt, (1,3,2))[:,:,:], permutedims(Wet[4], (1,3,2))[:,:,[2,4,3,1]]; xtitle="", ytitle="", wellnames=wellnames, keyword="noise-signals", xmax=100, ymax=1., gm=[Gadfly.Guide.manual_color_key("Sources", ["S1", "S2", "S3", "Background"], NTFk.colors[1:4]), Gadfly.Theme(major_label_font_size=16Gadfly.pt, key_label_font_size=14Gadfly.pt, minor_label_font_size=12Gadfly.pt)])
 display(Ht)
 display(Het[4][[2,3,1,4],:])
 
-Xe = NMFk.mixmatchcompute(X, Wet[4], Het[4])
-NTFk.plot2d(permutedims(X, (1,3,2))/1000, permutedims(Xe, (1,3,2))/1000; xtitle="", ytitle="", wellnames=wellnames, keyword="noise-species", xmax=100, ymin=0, ymax=1., gm=[Gadfly.Guide.manual_color_key("Species", ["A", "B", "C", "D"], NTFk.colors[1:4]), Gadfly.Theme(major_label_font_size=16Gadfly.pt, key_label_font_size=14Gadfly.pt, minor_label_font_size=12Gadfly.pt)])
+Xe = NMFk.mixmatchcompute(Xn, Wet[4], Het[4])
+NTFk.plot2d(permutedims(Xn, (1,3,2))/1000, permutedims(Xe, (1,3,2))/1000; xtitle="", ytitle="", wellnames=wellnames, keyword="noise-species", xmax=100, ymin=0, ymax=1., gm=[Gadfly.Guide.manual_color_key("Species", ["A", "B", "C", "D"], NTFk.colors[1:4]), Gadfly.Theme(major_label_font_size=16Gadfly.pt, key_label_font_size=14Gadfly.pt, minor_label_font_size=12Gadfly.pt)])
