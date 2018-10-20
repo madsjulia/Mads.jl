6afa09510a22995a26272e037fd041279f4a88d4
monty <velimir.vesselinov@gmail.com>
2018-10-20 10:55:46 -0600

    minor

--- examples/bigdt/runtests.jl
+++ examples/bigdt/runtests.jl
@@ -4,41 +4,41 @@ import FileIO
 using Base.Test
 
 if haskey(ENV, "MADS_NO_BIGUQ") || !isdefined(Mads, :dobigdt)
-	return
-end
-
-problemdir = Mads.getmadsdir()
-workdir = joinpath(Mads.madsdir, "examples", "bigdt")
-
-md = Dict()
-if isdefined(Mads, :pyyaml) && Mads.pyyaml != PyCall.PyNULL()
-	@Mads.stderrcapture md = Mads.loadmadsfile(joinpath(problemdir, "source_termination.mads"))
+	warn("BIGUQ cannot be tested!")
 else
-	@Mads.stderrcapture md = Mads.loadmadsfile(joinpath(problemdir, "source_termination_json.mads"), format="json") # for testing only
-end
+	problemdir = Mads.getmadsdir()
+	workdir = joinpath(Mads.madsdir, "examples", "bigdt")
 
-nsample = 10
-bigdt_results = Mads.dobigdt(md, nsample; maxHorizon = 0.8, numlikelihoods = 2)
+	md = Dict()
+	if isdefined(Mads, :pyyaml) && Mads.pyyaml != PyCall.PyNULL()
+		@Mads.stderrcapture md = Mads.loadmadsfile(joinpath(problemdir, "source_termination.mads"))
+	else
+		@Mads.stderrcapture md = Mads.loadmadsfile(joinpath(problemdir, "source_termination_json.mads"), format="json") # for testing only
+	end
 
-if isdefined(:Gadfly) && !haskey(ENV, "MADS_NO_GADFLY")
-	filenameroot = joinpath(problemdir, "source_termination-robustness-$nsample")
-	Mads.plotrobustnesscurves(md, bigdt_results; filename=filenameroot)
-	Mads.rmfile(joinpath(problemdir, "source_termination-robustness-10.svg"))
-	Mads.plotrobustnesscurves(md, bigdt_results)
-	Mads.rmfile(joinpath(problemdir, "source_termination-robustness.svg"))
-end
+	nsample = 10
+	bigdt_results = Mads.dobigdt(md, nsample; maxHorizon = 0.8, numlikelihoods = 2)
 
-# If enabled, save output as test file
-if Mads.create_tests
-	d = joinpath(workdir, "test_results")
-	Mads.mkdir(d)
-	FileIO.save(joinpath(d, "bigdt_results.jld2"), "bigdt_results", bigdt_results)
-end
+	if isdefined(:Gadfly) && !haskey(ENV, "MADS_NO_GADFLY")
+		filenameroot = joinpath(problemdir, "source_termination-robustness-$nsample")
+		Mads.plotrobustnesscurves(md, bigdt_results; filename=filenameroot)
+		Mads.rmfile(joinpath(problemdir, "source_termination-robustness-10.svg"))
+		Mads.plotrobustnesscurves(md, bigdt_results)
+		Mads.rmfile(joinpath(problemdir, "source_termination-robustness.svg"))
+	end
 
-good_bigdt_results = FileIO.load(joinpath(workdir, "test_results", "bigdt_results.jld2"), "bigdt_results")
+	# If enabled, save output as test file
+	if Mads.create_tests
+		d = joinpath(workdir, "test_results")
+		Mads.mkdir(d)
+		FileIO.save(joinpath(d, "bigdt_results.jld2"), "bigdt_results", bigdt_results)
+	end
 
-# Testing for bigdt
-@testset "Bigdt" begin
-	@test isapprox(bigdt_results["maxfailureprobs"], good_bigdt_results["maxfailureprobs"], atol=1e-6)
-	@test isapprox(bigdt_results["horizons"], good_bigdt_results["horizons"], atol=1e-6)
+	good_bigdt_results = FileIO.load(joinpath(workdir, "test_results", "bigdt_results.jld2"), "bigdt_results")
+
+	# Testing for bigdt
+	@testset "Bigdt" begin
+		@test isapprox(bigdt_results["maxfailureprobs"], good_bigdt_results["maxfailureprobs"], atol=1e-6)
+		@test isapprox(bigdt_results["horizons"], good_bigdt_results["horizons"], atol=1e-6)
+	end
 end
