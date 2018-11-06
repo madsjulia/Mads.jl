8edca949ea57f42fd0e4c03c918ee0a044cf2d1b
monty <velimir.vesselinov@gmail.com>
2018-11-06 10:15:02 -0700

    ode fix

--- examples/ode/runtests.jl
+++ examples/ode/runtests.jl
@@ -21,61 +21,57 @@ Mads.graphoff()
 	return func
 end
 
-if isdefined(Mads, :OrdinaryDiffEq) && Mads.pkgversion("OrdinaryDiffEq") >= v"3.1.0"
-	# load parameter data from MADS YAML file
-	Mads.madsinfo("Loading data ...")
-	workdir = Mads.getmadsdir() # get the directory where the problem is executed
-	if workdir == "."
-		workdir = joinpath(Mads.madsdir, "examples", "ode")
+# load parameter data from MADS YAML file
+Mads.madsinfo("Loading data ...")
+workdir = Mads.getmadsdir() # get the directory where the problem is executed
+if workdir == "."
+	workdir = joinpath(Mads.madsdir, "examples", "ode")
+end
+
+md = Mads.loadmadsfile(joinpath(workdir, "ode.mads"))
+rootname = Mads.getmadsrootname(md)
+
+# get parameter keys
+paramkeys = Mads.getparamkeys(md)
+# Mads.showparameters(md)
+
+# create parameter dictionary
+paramdict = OrderedCollections.OrderedDict(zip(paramkeys, map(key->md["Parameters"][key]["init"], paramkeys)))
+
+@Test.testset "ODE Solver" begin
+	# create a function for the ODE solver
+	funcosc = makefunc(paramdict)
+	Mads.madsinfo("Solve ODE ...")
+	t = collect(0:.1:100)
+	initialconditions = [1., 0.]
+	odeprob = OrdinaryDiffEq.ODEProblem(funcosc, initialconditions, (0.0, 100.0))
+	sol = OrdinaryDiffEq.solve(odeprob, OrdinaryDiffEq.Tsit5(), saveat=t)
+	ys = convert(Array, sol)'
+
+	# Write good test results to directory
+	if Mads.create_tests
+		dirt = joinpath(workdir, "test_results")
+		Mads.mkdir(dirt)
+
+		FileIO.save(joinpath(dirt, "ode_solver_t.jld2"), "t", t)
+		FileIO.save(joinpath(dirt, "ode_solver_y.jld2"), "ys", ys)
 	end
 
-	md = Mads.loadmadsfile(joinpath(workdir, "ode.mads"))
-	rootname = Mads.getmadsrootname(md)
+	good_ode_t = FileIO.load(joinpath(workdir, "test_results", "ode_solver_t.jld2"), "t")
+	good_ode_ys = FileIO.load(joinpath(workdir, "test_results", "ode_solver_y.jld2"), "ys")
 
-	# get parameter keys
-	paramkeys = Mads.getparamkeys(md)
-	# Mads.showparameters(md)
+	@Test.test isapprox(t, good_ode_t, atol=1e-6)
+	@Test.test isapprox(ys, good_ode_ys, atol=1e-6)
 
-	# create parameter dictionary
-	paramdict = OrderedCollections.OrderedDict(zip(paramkeys, map(key->md["Parameters"][key]["init"], paramkeys)))
-
-	@Test.testset "ODE Solver" begin
-		# create a function for the ODE solver
-		funcosc = makefunc(paramdict)
-		Mads.madsinfo("Solve ODE ...")
-		t = collect(0:.1:100)
-		initialconditions = [1., 0.]
-		odeprob = OrdinaryDiffEq.ODEProblem(funcosc, initialconditions, (0.0, 100.0))
-		sol = OrdinaryDiffEq.solve(odeprob, OrdinaryDiffEq.Tsit5(), saveat=t)
-		ys = convert(Array, sol)'
-
-		# Write good test results to directory
-		if Mads.create_tests
-			dirt = joinpath(workdir, "test_results")
-			Mads.mkdir(dirt)
-
-			FileIO.save(joinpath(dirt, "ode_solver_t.jld2"), "t", t)
-			FileIO.save(joinpath(dirt, "ode_solver_y.jld2"), "ys", ys)
-		end
-
-		good_ode_t = FileIO.load(joinpath(workdir, "test_results", "ode_solver_t.jld2"), "t")
-		good_ode_ys = FileIO.load(joinpath(workdir, "test_results", "ode_solver_y.jld2"), "ys")
-
-		@Test.test isapprox(t, good_ode_t, atol=1e-6)
-		@Test.test isapprox(ys, good_ode_ys, atol=1e-6)
-
-		# create an observation dictionary in the MADS problem dictionary
-		Mads.madsinfo("Create MADS Observations ...")
-		Mads.createobservations!(md, t, ys[:,1], weight = 10)
-		Mads.createobservations!(md, t, ys[:,1], weight_type = "inverse", logtransform=true)
-		Mads.createobservations!(md, Dict("a"=>1,"c"=>1), weight = 10)
-		Mads.createobservations!(md, Dict("a"=>1,"c"=>1), weight_type = "inverse", logtransform=true)
-		Mads.createobservations!(md, t, ys[:,1])
-		# Mads.madsinfo("Show MADS Observations ...")
-		# Mads.showobservations(md)
-	end
-else
-	@warn("OrdinaryDiffEq.jl is missing")
+	# create an observation dictionary in the MADS problem dictionary
+	Mads.madsinfo("Create MADS Observations ...")
+	Mads.createobservations!(md, t, ys[:,1], weight = 10)
+	Mads.createobservations!(md, t, ys[:,1], weight_type = "inverse", logtransform=true)
+	Mads.createobservations!(md, Dict("a"=>1,"c"=>1), weight = 10)
+	Mads.createobservations!(md, Dict("a"=>1,"c"=>1), weight_type = "inverse", logtransform=true)
+	Mads.createobservations!(md, t, ys[:,1])
+	# Mads.madsinfo("Show MADS Observations ...")
+	# Mads.showobservations(md)
 end
 
 Mads.veryquietoff()
