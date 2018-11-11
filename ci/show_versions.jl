e75eec18d6725f6c0a62d7ba91fbb082302b77f1
monty <velimir.vesselinov@gmail.com>
2018-11-11 15:17:26 -0700

    minor

--- ci/show_versions.jl
+++ ci/show_versions.jl
@@ -1,10 +1,3 @@
-if VERSION >= v"0.7.0-"
-    using Pkg
-    show(stdout, "text/plain", Pkg.installed())
-    println()
-else
-    for line in split(strip(readstring("REQUIRE")), '\n')[2:end]
-        name = split(line)[1]
-        println(name, "\t", Pkg.installed(name))
-    end
-end
+import Pkg
+show(stdout, "text/plain", Pkg.installed())
+println()
