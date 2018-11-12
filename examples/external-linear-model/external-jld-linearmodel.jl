564ddaaa3809f9015322432dcfe5db37cb56733d
monty <velimir.vesselinov@gmail.com>
2018-11-12 13:38:03 -0700

    major fixes; model coupling now works

--- examples/external-linear-model/external-jld-linearmodel.jl
+++ examples/external-linear-model/external-jld-linearmodel.jl
@@ -1,16 +1,12 @@
 import JLD2
 import FileIO
 
-if VERSION >= v"0.7"
-	using OrderedCollections
-else
-	using DataStructures
-end
+using OrderedCollections
 
 parameters = FileIO.load("parameters.jld2") # JLD file created to write current model parameters
 
 f(t) = parameters["a"] * t - parameters["b"] # a * t - b; linear model
 times = 1:4
-predictions = OrderedDict(zip(map(i -> string("o", i), times), map(f, times)))
+predictions = OrderedCollections.OrderedDict(zip(map(i -> string("o", i), times), map(f, times)))
 
 FileIO.save("predictions.jld2", predictions) # JLD file created to write current model predictions
