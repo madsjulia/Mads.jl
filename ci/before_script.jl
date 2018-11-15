0c20df1bd078891211dca949ebfe40b03e74f614
monty <velimir.vesselinov@gmail.com>
2018-11-15 06:58:46 -0700

    CI fixes

--- ci/before_script.jl
+++ ci/before_script.jl
@@ -1,7 +1,34 @@
-import Pkg
+Pkg = Base.require(Base.PkgId(Base.UUID(0x44cfe95a1eb252eab672e2afdf69b78f), "Pkg"))
 
-@info "Pkg.clone(pwd())"
-Pkg.clone(pwd())
+try
+	@info "Pkg.build(PyCall)"
+	ENV["PYTHON"] = ""
+	Pkg.build("PyCall")
+
+	import PyCall
+
+	@info "PyCall/deps/build.log:"
+	print(read(joinpath(dirname(dirname(pathof(PyCall))), "deps", "build.log"), String))
+catch
+	@warn "PyCall does not work"
+	ENV["MADS_NO_PYTHON"] = ""
+end
+
+try
+	@info "Pkg.build(PyPlot)"
+	Pkg.build("PyPlot")
+
+	import PyPlot
+
+	@info "PyPlot/deps/build.log:"
+	print(read(joinpath(dirname(dirname(pathof(PyPlot))), "deps", "build.log"), String))
+catch
+	@warn "PyPlot does not work"
+	ENV["MADS_NO_PYPLOT"] = ""
+end
+
+@info "Pkg.add(Pkg.PackageSpec(url=pwd()))"
+Pkg.add(Pkg.PackageSpec(url=pwd()))
 
 @info "Pkg.build(Mads)"
 Pkg.build("Mads")
@@ -10,22 +37,6 @@ import Mads
 
 @info "Mads/deps/build.log:"
 print(read(joinpath(dirname(dirname(pathof(Mads))), "deps", "build.log"), String))
-
-@info "Pkg.build(PyCall)"
-Pkg.build("PyCall")
-
-import PyCall
-
-@info "PyCall/deps/build.log:"
-print(read(joinpath(dirname(dirname(pathof(PyCall))), "deps", "build.log"), String))
-
-@info "Pkg.build(PyPlot)"
-Pkg.build("PyPlot")
-
-import PyPlot
-
-@info "PyPlot/deps/build.log:"
-print(read(joinpath(dirname(dirname(pathof(PyPlot))), "deps", "build.log"), String))
 
 @info "Package versions"
 show(stdout, "text/plain", Pkg.installed())
