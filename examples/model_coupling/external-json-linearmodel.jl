cc0265851955255da29de6f03e4cdb516566d252
monty <velimir.vesselinov@gmail.com>
2019-01-20 10:51:08 -0700

    important fixes

--- examples/model_coupling/external-json-linearmodel.jl
+++ examples/model_coupling/external-json-linearmodel.jl
@@ -1,13 +1,14 @@
 import JSON
+import OrderedCollections
 
-using OrderedCollections
-
-parameters = JSON.parsefile("parameters.json"; dicttype=OrderedDict)
+parameters = JSON.parsefile("parameters.json"; dicttype=OrderedCollections.OrderedDict)
 
 f(t) = parameters["a"] * t - parameters["b"] # a * t - b; linear model
 times = 1:4
+@show map(i -> string("o", i), times), map(f, times)
 predictions = OrderedCollections.OrderedDict(zip(map(i -> string("o", i), times), map(f, times)))
 
+@show predictions
 jo = open("predictions.json", "w")
 JSON.print(jo, predictions)
 close(jo)
