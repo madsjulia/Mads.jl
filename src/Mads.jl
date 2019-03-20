2a8c6c377388ea9ffadda71e8f959bc9cac8f11a
monty <velimir.vesselinov@gmail.com>
2019-03-20 11:25:02 -0600

    dir fix

--- src/Mads.jl
+++ src/Mads.jl
@@ -165,7 +165,7 @@ global long_tests = false # execute long tests
 global madsservers = ["madsmax", "madsmen", "madsdam", "madszem", "madskil", "madsart", "madsend"]
 global madsservers2 = ["madsmin"; map(i->(@Printf.sprintf "mads%02d" i), 1:18); "es05"; "es06"]
 global nprocs_per_task_default = 1
-const madsdir = splitdir(splitdir(Base.source_path())[1])[1]
+const madsdir = splitdir(splitdir(pathof(Mads))[1])[1]
 
 if haskey(ENV, "MADS_LONG_TESTS")
 	global long_tests = true
