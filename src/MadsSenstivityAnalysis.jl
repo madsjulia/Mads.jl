f926584373532e37f37064d2f94f7f9d5b134752
monty <velimir.vesselinov@gmail.com>
2021-01-05 17:27:09 -0700

    julia 1.5.3 fixes

--- src/MadsSenstivityAnalysis.jl
+++ src/MadsSenstivityAnalysis.jl
@@ -1067,7 +1067,7 @@ argtext=Dict("df"=>"dataframe")))
 function deleteNaN!(df::DataFrames.DataFrame)
 	for i in 1:size(df, 2)
 		if typeof(df[!, i][1]) <: Number
-			DataFrames.deleterows!(df, findall(isnan.(df[!, i][:])))
+			delete!(df, findall(isnan.(df[!, i][:])))
 			if size(df, 1) == 0
 				return
 			end
