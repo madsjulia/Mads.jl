424e9d9b5990b60a6b280f360c35f7d2d704dd79
monty <velimir.vesselinov@gmail.com>
2021-12-03 09:15:45 -0700

    cleanup

--- src/MadsCMads.jl
+++ src/MadsCMads.jl
@@ -16,7 +16,7 @@ function readobservations_cmads(madsdata::AbstractDict)
 	observations = OrderedCollections.OrderedDict{String,Float64}(zip(obsids, zeros(length(obsids))))
 	for instruction in madsdata["Instructions"]
 		obs = cmadsins_obs(obsids, instruction["ins"], instruction["read"])
-		#this loop assumes that cmadsins_obs gives a zero value if the obs is not found, and that each obs will appear only once
+		# this loop assumes that cmadsins_obs gives a zero value if the obs is not found, and that each obs will appear only once
 		for obsid in obsids
 			observations[obsid] += obs[obsid]
 		end
@@ -38,13 +38,11 @@ Return:
 """
 function cmadsins_obs(obsid::AbstractVector, instructionfilename::AbstractString, inputfilename::AbstractString)
 	n = length(obsid)
-	obsval = zeros(n) # initialize to 0
-	obscheck = -1 * ones(n) # initialize to -1
+	obsval = zeros(n)
+	obscheck = -1 * ones(n)
 	debug = 0 # setting debug level 0 or 1 works
 	# int ins_obs( int nobs, char **obs_id, double *obs, double *check, char *fn_in_t, char *fn_in_d, int debug );
-	result = ccall((:ins_obs, "libmads"), Int32,
-					(Int32, Ptr{Ptr{UInt8}}, Ptr{Float64}, Ptr{Float64}, Ptr{UInt8}, Ptr{UInt8}, Int32),
-					n, obsid, obsval, obscheck, instructionfilename, inputfilename, debug)
+	result = ccall((:ins_obs, "libmads"), Int32, (Int32, Ptr{Ptr{UInt8}}, Ptr{Float64}, Ptr{Float64}, Ptr{UInt8}, Ptr{UInt8}, Int32), n, obsid, obsval, obscheck, instructionfilename, inputfilename, debug)
 	observations = Dict{String, Float64}(zip(obsid, obsval))
 	return observations
 end
