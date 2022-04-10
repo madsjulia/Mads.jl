5d3725be191363a5a8a5c862b9f2f2f36e0c21ca
monty <velimir.vesselinov@gmail.com>
2022-04-09 21:27:41 -0600

    win

--- examples/model_coupling/external-linearmodel+template+instruction-l1.jl
+++ examples/model_coupling/external-linearmodel+template+instruction-l1.jl
@@ -2,15 +2,11 @@ using OrderedCollections
 using DelimitedFiles
 
 i = open("parameters.dat", "r")
-param = readdlm(i)
+param = DelimitedFiles.readdlm(i)
 close(i)
 a = param[1]
 b = param[2]
 f(t) = a * t - b # a * t - b
 times = 0:4
-@show a
-@show b
-@show map(f, times)
-predictions = OrderedDict{String, Float64}(zip(map(i->string("o", i), times), map(f, times)))
-@show predictions
+predictions = OrderedCollections.OrderedDict{String, Float64}(zip(map(i->string("o", i), times), map(f, times)))
 writedlm("observations.dat", predictions)
