bafc87b4fb77c85d7164ddcd82245e2a605cbc0b
monty <velimir.vesselinov@gmail.com>
2022-08-03 17:43:46 -0600

    jump

--- examples/minimization/compare.jl
+++ examples/minimization/compare.jl
@@ -6,15 +6,16 @@ import Random
 
 Random.seed!(1)
 xtrue = 1.3
-d = collect(range(0, stop=3, length=100))
-y = exp.(-d * xtrue) + 0.05 * randn(size(d))
+btrue = 3
+d = collect(range(0; stop=3, length=100))
+y = exp.(-d .* xtrue) .+ btrue .+ 0.05 * randn(size(d))
 Mads.plotseries(y; xaxis=d)
 
 function func_jump(x...)
 	sum(((exp.(-d * x[1]) .+ x[2]) .- y) .^ 2)
 end
 
-function func_mads2(x)
+function func_mads(x)
 	exp.(-d .* x[1]) .+ x[2] .- y
 end
 
@@ -27,7 +28,7 @@ JuMP.register(m, :func_jump, nvar, func_jump; autodiff=true)
 @JuMP.variable(m, x[1:nvar]; start=2)
 @JuMP.NLobjective(m, Min, func_jump(x...))
 @time JuMP.optimize!(m)
-@show xtrue, JuMP.value.(x)
+@show xtrue, btrue, JuMP.value.(x)
 
 @info("Ipopt")
 m = JuMP.Model(Ipopt.Optimizer)
@@ -35,8 +36,8 @@ JuMP.register(m, :func_jump, nvar, func_jump; autodiff=true)
 @JuMP.variable(m, x[1:nvar]; start=2)
 @JuMP.NLobjective(m, Min, func_jump(x...))
 @time JuMP.optimize!(m)
-@show xtrue, JuMP.value.(x)
+@show xtrue, btrue, JuMP.value.(x)
 
 @info("Mads")
 @time minimizer, results = Mads.minimize(func_mads2, [2., 2.]; upperbounds=[4., 4.], lowerbounds=[0., 0.])
-@show xtrue, minimizer
+@show xtrue, btrue, minimizer
