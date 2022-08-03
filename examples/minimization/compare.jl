61f79e5c31bd9e58fa279add4dbd61eb3c47e51e
monty <velimir.vesselinov@gmail.com>
2022-08-03 10:54:05 -0600

    JuMP

--- examples/minimization/compare.jl
+++ examples/minimization/compare.jl
@@ -2,10 +2,13 @@ import Mads
 import JuMP
 import Ipopt
 import NLopt
+import Random
 
 Random.seed!(1)
+xtrue = 1.3
 d = collect(range(0, stop=3, length=100))
-y = exp.(-1.3*d) + 0.05*randn(size(d))
+y = exp.(-d * xtrue) + 0.05 * randn(size(d))
+Mads.plotseries(y; xaxis=d)
 
 function func_nlopt(r...)
 	x = collect(r)
@@ -19,20 +22,22 @@ end
 nvar = 1
 
 @info("NLopt")
-m = JuMP.Model(solver=NLopt.NLoptSolver(algorithm=:LD_LBFGS))
-JuMP.register(m, :func_nlopt, nvar, func_nlopt, autodiff=true)
-@JuMP.variable(m, x[i=1:nvar], start=2)
-JuMP.setNLobjective(m, :Min, Expr(:call, :func_nlopt, [x[i] for i=1:nvar]...))
+m = JuMP.Model(NLopt.Optimizer)
+JuMP.set_optimizer_attribute(m, "algorithm", :LD_MMA)
+JuMP.register(m, :func_nlopt, nvar, func_nlopt; autodiff=true)
+@JuMP.variable(m, x[1:nvar]; start=2)
+@JuMP.NLobjective(m, Min, func_nlopt(x[1]))
 @time JuMP.optimize!(m)
-JuMP.value.(x)
+@show xtrue, JuMP.value.(x)
 
 @info("Ipopt")
-m = JuMP.Model(solver=Ipopt.IpoptSolver())
-JuMP.register(m, :func_nlopt, nvar, func_nlopt, autodiff=true)
-@JuMP.variable(m, x[i=1:nvar], start=2)
-JuMP.setNLobjective(m, :Min, Expr(:call, :func_nlopt, [x[i] for i=1:nvar]...))
+m = JuMP.Model(Ipopt.Optimizer)
+JuMP.register(m, :func_nlopt, nvar, func_nlopt; autodiff=true)
+@JuMP.variable(m, x[1:nvar]; start=2)
+@JuMP.NLobjective(m, Min, func_nlopt(x[1]))
 @time JuMP.optimize!(m)
-JuMP.value.(x)
+@show xtrue, JuMP.value.(x)
 
 @info("Mads")
 @time minimizer, results = Mads.minimize(func_mads, [2.]; upperbounds=[4.], lowerbounds=[0.])
+@show xtrue, minimizer
