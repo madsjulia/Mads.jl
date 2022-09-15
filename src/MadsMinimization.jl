7cfbc2cf24465ef4fe6fb2ec2ed74504c1ffd769
monty <velimir.vesselinov@gmail.com>
2022-09-14 22:09:32 -0600

    LM fix

--- src/MadsMinimization.jl
+++ src/MadsMinimization.jl
@@ -11,7 +11,9 @@ keytext=Dict("lowerbounds"=>"vector of lower bounds of the function arguments",
 			"logtransformed"=>"boolean vector of the log-transformed function arguments",
 			"tolX"=>"parameter space tolerance [default=`1e-4`]",
 			"tolG"=>"parameter space update tolerance [default=`1e-6`]",
-			"tolOF"=>"objective function tolerance [default=`1e-3`]",
+			"tolOF"=>"objective function update tolerance [default=`1e-3`]",
+			"tolOFcount"=>"number of Jacobian runs with small objective function change [default=`5`]",
+			"minOF"=>"objective function update tolerance [default=`1e-3`]",
 			"maxEval"=>"maximum number of model evaluations [default=`1000`]",
 			"maxIter"=>"maximum number of optimization iterations [default=`100`]",
 			"maxJacobians"=>"maximum number of Jacobian solves [default=`100`]",
@@ -26,7 +28,7 @@ Returns:
 - vector with the optimal parameter values at the minimum
 - optimization algorithm results (e.g. results.minimizer)
 """
-function minimize(f::Function, x::AbstractVector; lowerbound::Number=-1e+8, lowerbounds::Array{Float64,1}=fill(convert(Float64, lowerbound), length(x)), upperbound::Number=1e+8, upperbounds::Array{Float64,1}=fill(convert(Float64, upperbound), length(x)), logtransform::Bool=false, logtransformed::Union{BitArray{1},Array{Bool,1}}=fill(false, length(x)), tolX::Number=1e-4, tolG::Number=1e-6, tolOF::Number=1e-3, maxEval::Integer=1000, maxIter::Integer=100, maxJacobians::Integer=100, lambda::Number=100.0, lambda_mu::Number=10.0, np_lambda::Integer=10, show_trace::Bool=false, sindx::Float64=0.1, quiet::Bool=true)
+function minimize(f::Function, x::AbstractVector; lowerbound::Number=-1e+8, lowerbounds::Array{Float64,1}=fill(convert(Float64, lowerbound), length(x)), upperbound::Number=1e+8, upperbounds::Array{Float64,1}=fill(convert(Float64, upperbound), length(x)), logtransform::Bool=false, logtransformed::Union{BitArray{1},Array{Bool,1}}=fill(false, length(x)), tolX::Number=1e-4, tolG::Number=1e-6, tolOF::Number=1e-3, tolOFcount::Integer=5, minOF::Number=1e-3, maxEval::Integer=1000, maxIter::Integer=100, maxJacobians::Integer=100, lambda::Number=100.0, lambda_mu::Number=10.0, np_lambda::Integer=10, show_trace::Bool=false, sindx::Float64=0.1, quiet::Bool=true)
 	!quiet && Mads.quietoff()
 	f_lm, g_lm, o_lm = Mads.makelmfunctions(f)
 	lb = copy(lowerbounds)
@@ -42,7 +44,7 @@ function minimize(f::Function, x::AbstractVector; lowerbound::Number=-1e+8, lowe
 	initparams = Mads.asinetransform(x, lb, ub, indexlogtransformed)
 	f_lm_sin = Mads.sinetransformfunction(f_lm, lb, ub, indexlogtransformed)
 	g_lm_sin = Mads.sinetransformgradient(g_lm, lb, ub, indexlogtransformed; sindx=sindx)
-	results = Mads.levenberg_marquardt(f_lm_sin, g_lm_sin, initparams, o_lm; tolX=tolX, tolG=tolG, tolOF=tolOF, maxEval=maxEval, maxIter=maxIter, maxJacobians=maxJacobians, lambda=lambda, lambda_mu=lambda_mu, np_lambda=np_lambda, show_trace=show_trace)
+	results = Mads.levenberg_marquardt(f_lm_sin, g_lm_sin, initparams, o_lm; tolX=tolX, tolG=tolG, tolOF=tolOF, tolOFcount=tolOFcount, minOF=minOF, maxEval=maxEval, maxIter=maxIter, maxJacobians=maxJacobians, lambda=lambda, lambda_mu=lambda_mu, np_lambda=np_lambda, show_trace=show_trace)
 	minimizer = Mads.sinetransform(results.minimizer, lb, ub, indexlogtransformed)
 	!quiet && Mads.quieton()
 	return minimizer, results
