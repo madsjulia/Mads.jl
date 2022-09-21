f1f4ee22b4e4d9441a2a90655578e448c1fd9014
monty <velimir.vesselinov@gmail.com>
2022-09-21 10:54:59 -0600

    plot

---
+++ scripts/plotlyjs.jl
@@ -0,0 +1,25 @@
+nm = tempname()
+url = "https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv"
+download(url, nm)
+df = CSV.read(nm, DataFrames.DataFrame)
+rm(nm)
+
+trace = PlotlyJS.scattergeo(;locationmode="USA-states",
+					lat=df[!, :lat],
+					lon=df[!, :lon],
+					hoverinfo="text",
+					text=[string(x[:name], " pop: ", x[:pop]) for x in eachrow(df)],
+					marker_size=df[!, :pop] / 50_000,
+					marker_line_color="black", marker_line_width=2)
+
+geo = PlotlyJS.attr(scope="usa",
+			projection_type="albers usa",
+			showland=true,
+			landcolor="rgb(217, 217, 217)",
+			subunitwidth=1,
+			countrywidth=1,
+			subunitcolor="rgb(255,255,255)",
+			countrycolor="rgb(255,255,255)")
+
+layout = PlotlyJS.Layout(;title="2014 US City Populations", showlegend=false, geo=geo)
+PlotlyJS.plot(trace, layout)
---
+++ scripts/stipple.jl
@@ -0,0 +1,42 @@
+import Stipple
+import StipplePlotly
+
+@Stipple.reactive! mutable struct Model <: Stipple.ReactiveModel
+	data::Stipple.R{Vector{StipplePlotly.PlotData}} = [
+		StipplePlotly.PlotData(
+			plot = StipplePlotly.Charts.PLOT_TYPE_SCATTERMAPBOX,
+			lon = [-74, -70, -70, -74],
+			lat = [47, 47, 45, 45],
+			marker = StipplePlotly.PlotDataMarker(size=10, color="red")
+		)
+		StipplePlotly.PlotData(
+			plot = StipplePlotly.Charts.PLOT_TYPE_SCATTERMAPBOX,
+			lon = [-74, -70, -70, -74],
+			lat = [47.7, 47.5, 45.5, 45.5],
+			marker = StipplePlotly.PlotDataMarker(size=10, color="orange")
+		)
+	]
+	layout::R{PlotLayout} = StipplePlotly.PlotLayout(
+		mapbox = PlotLayoutMapbox(style="stamen-terrain", zoom =5,  center = MCenter(-73, 46)),
+		showlegend= false,
+		height= 450,
+		width= 600
+	)
+	config::Stipple.R{StipplePlotly.PlotConfig} = StipplePlotly.PlotConfig()
+end
+
+model = Model |> Stipple.init
+
+function ui(model)
+	Stipple.page(
+		model,
+		class = "container",
+		StipplePlotly.plot(:data, layout = :layout, config = :config)
+	)
+end
+
+route("/") do
+	Stipple.init(Model) |> ui |> html
+end
+
+Stipple.up()
