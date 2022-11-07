42dc99284ef2d5843f898c5fb59dca00a2ab9ab4
monty <velimir.vesselinov@gmail.com>
2022-11-07 16:53:42 -0700

    stipple checkbox

--- scripts/stipple_checkbox.jl
+++ scripts/stipple_checkbox.jl
@@ -3,27 +3,41 @@ import StippleUI
 
 @Stipple.reactive! mutable struct Model <: Stipple.ReactiveModel
 	click::Stipple.R{Int64} = 0
-	checks::Stipple.R{Vector{Bool}} = falses(0)
+	checks::Stipple.R{Vector{Bool}} = falses(2)
 end
 
 function ui(model)
-	Stipple.on(model.click) do (_...)
-		push!(model.checks[], false)
-		@show model.checks[] # the array increases
-		Stipple.notify(model.checks)
-	end
 	Stipple.page(model, class="container", [
 		Stipple.cell(class="st-module", [
 			Stipple.button("Click", @StippleUI.click("click += 1"))
 		]),
 		StippleUI.separator(),
-		# However, length(model.checks[]) stays equal to zero
-		[StippleUI.checkbox("Checkbox $i", Symbol("checks[$(i-1)]")) for i in 1:length(model.checks[])]...,
-
+		# Stipple.h5("Clicks: $(length(model.checks[]))"), # this does not work
+		Stipple.h5("Clicks: {{this.checks.length}}"),
+		Stipple.Html.div(
+			StippleUI.quasar(:checkbox, label = Stipple.R"'checkbox ' + index", fieldname = Stipple.R"checks[index]"),
+			Stipple.@recur("(item, index) in checks")
+		)
+		# StippleUI.checkbox(Stipple.R"'checkbox ' + index", Stipple.R"checks[index]"),
+		# Stipple.@recur("(item, index) in checks")
 	])
 end
 
-model = Stipple.init(Model)
+function handlers(model)
+	Stipple.on(model.isready) do ready
+		ready || return
+		push!(model)
+	end
+
+	Stipple.on(model.click) do (_...)
+		push!(model.checks[], false)
+		@show model.checks[] # the array increases
+		Stipple.notify(model.checks) # notify does not work
+	end
+	return model
+end
+
+model = handlers(Stipple.init(Model))
 
 Stipple.route("/") do
 	Stipple.html(ui(model); context = @__MODULE__)
