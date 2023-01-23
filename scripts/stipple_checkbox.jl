fb643123c9cc3997c190789ddbb1ea683a092dba
monty <velimir.vesselinov@gmail.com>
2023-01-23 09:55:31 -0700

    stipple

--- scripts/stipple_checkbox.jl
+++ scripts/stipple_checkbox.jl
@@ -1,47 +1,43 @@
 import Stipple
 import StippleUI
 
-@Stipple.reactive! mutable struct Model <: Stipple.ReactiveModel
+@Stipple.reactive! mutable struct UIModelCheckBox <: Stipple.ReactiveModel
 	click::Stipple.R{Int64} = 0
 	checks::Stipple.R{Vector{Bool}} = falses(2)
 end
 
-function ui(model)
-	Stipple.page(model, class="container", [
+function ui(ui_model_check_box)
+	Stipple.page(ui_model_check_box, class="container", [
 		Stipple.cell(class="st-module", [
 			Stipple.button("Click", @StippleUI.click("click += 1"))
 		]),
 		StippleUI.separator(),
-		# Stipple.h5("Clicks: $(length(model.checks[]))"), # this does not work
 		Stipple.h5("Clicks: {{this.checks.length}}"),
 		Stipple.Html.div(
 			StippleUI.quasar(:checkbox, label = Stipple.R"'checkbox ' + index", fieldname = Stipple.R"checks[index]"),
 			Stipple.@recur("(item, index) in checks")
 		)
-		# StippleUI.checkbox(Stipple.R"'checkbox ' + index", Stipple.R"checks[index]"),
-		# Stipple.@recur("(item, index) in checks")
 	])
 end
 
-function handlers(model)
-	Stipple.on(model.isready) do ready
+function handlers(ui_model_check_box)
+	Stipple.on(ui_model_check_box.isready) do ready
 		ready || return
-		push!(model)
+		push!(ui_model_check_box)
 	end
 
-	Stipple.on(model.click) do (_...)
-		push!(model.checks[], false)
-		@show model.checks[] # the array increases
-		Stipple.notify(model.checks) # notify does not work
+	Stipple.on(ui_model_check_box.click) do (_...)
+		push!(ui_model_check_box.checks[], false)
+		Stipple.notify(ui_model_check_box.checks)
 	end
-	return model
+	return ui_model_check_box
 end
 
-model = handlers(Stipple.init(Model))
+ui_model_check_box = handlers(Stipple.init(UIModelCheckBox))
 
 Stipple.route("/") do
-	Stipple.html(ui(model); context = @__MODULE__)
+	Stipple.html(ui(ui_model_check_box); context = @__MODULE__)
 end
 
 Stipple.down()
-Stipple.up(async = true)
+Stipple.up(; async=true)
