30e12e43eaf134835946826c00a4bd466012f550
monty <velimir.vesselinov@gmail.com>
2023-01-24 17:13:47 -0700

    cleanup

--- src/Mads.jl
+++ src/Mads.jl
@@ -3,7 +3,6 @@ __precompile__()
 """
 MADS: Model Analysis & Decision Support in Julia (Mads.jl v1.0) 2019
 
-http://mads.lanl.gov
 https://github.com/madsjulia
 
 Licensing: GPLv3: http://www.gnu.org/licenses/gpl-3.0.html
