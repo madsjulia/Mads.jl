e0cef5f59e8dacaef606acb5a6a0d6eb611fbd5a
monty <velimir.vesselinov@gmail.com>
2023-02-02 14:25:08 -0700

    test

--- scripts/stipple_checkbox.jl
+++ scripts/stipple_checkbox.jl
@@ -1,8 +1,16 @@
 import Stipple
 import StippleUI
 
+mutable struct Test
+	a::Int64
+	b::String
+	Test() = new(0, "")
+end
+
 @Stipple.reactive! mutable struct UIModelCheckBox <: Stipple.ReactiveModel
 	click::Stipple.R{Int64} = 0
+	test::Test = Test()
+	test2::Stipple.R{Test} = Test()
 	checks::Stipple.R{Vector{Bool}} = falses(2)
 end
 
@@ -13,6 +21,18 @@ function ui(ui_model_check_box)
 		]),
 		StippleUI.separator(),
 		Stipple.h5("Clicks: {{this.checks.length}}"),
+		Stipple.p([
+			"Clicks: "
+			Stipple.span(ui_model_check_box.click, @Stipple.text(:click))
+		  ]),
+		Stipple.h5("Test: $(ui_model_check_box.test)"),
+		Stipple.h5("Test: $(ui_model_check_box.test2[])"),
+		Stipple.p([
+			"Value: "
+			Stipple.span(ui_model_check_box.test2.a, @Stipple.text(:(test2.a)))
+		  ]),
+		Stipple.h5("Test: {{this.test.a}}"),
+		Stipple.h5("Test2: {{this.test2.a}}"),
 		Stipple.Html.div(
 			StippleUI.quasar(:checkbox, label = Stipple.R"'checkbox ' + index", fieldname = Stipple.R"checks[index]"),
 			Stipple.@recur("(item, index) in checks")
@@ -22,11 +42,15 @@ end
 
 function handlers(ui_model_check_box)
 	Stipple.on(ui_model_check_box.isready) do ready
+		ui_model_check_box.test.a = 5
+		ui_model_check_box.test2[].a = 6
 		ready || return
 		push!(ui_model_check_box)
 	end
 
 	Stipple.on(ui_model_check_box.click) do (_...)
+		ui_model_check_box.test.a += 1
+		ui_model_check_box.test2[].a += 1
 		push!(ui_model_check_box.checks[], false)
 		Stipple.notify(ui_model_check_box.checks)
 	end
