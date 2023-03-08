d0875629ccfbe0d2c9c379a35e9b88db214fca44
monty <velimir.vesselinov@gmail.com>
2023-03-08 08:47:07 -0700

    fix

--- src/MadsTryImport.jl
+++ src/MadsTryImport.jl
@@ -1,24 +1,23 @@
 "Try to import a module in Mads"
-macro tryimport(s::Symbol, domains::Symbol=:Mads)
+macro tryimport(s::Symbol, domain::Symbol=:Mads)
 	mname = string(s)
-	domain = eval(domains)
 	if !ispkgavailable(mname)
 		try
 			Pkg.add(mname)
 		catch
-			@info string("Module ", s, " is not available!")
+			@info("Module $(mname) is not available!")
 			return nothing
 		end
 	end
 	if !isdefined(domain, s)
-		importq = string(:(import $s))
-		warnstring = string("Module ", s, " cannot be imported!")
+		importq = string(:(import $(s)))
+		warnstring = "Module $(mname) cannot be imported!"
 		q = quote
 			try
-				Core.eval($domain, Meta.parse($importq))
+				Core.eval($(eval(domain)), Meta.parse($(importq)))
 			catch errmsg
 				printerrormsg(errmsg)
-				@warn($warnstring)
+				@warn($(warnstring))
 			end
 		end
 		return :($(esc(q)))
@@ -28,6 +27,6 @@ end
 "Try to import a module in Main"
 macro tryimportmain(s::Symbol)
 	quote
-		@Mads.tryimport $s Main
+		@Mads.tryimport $(s) Main
 	end
 end
