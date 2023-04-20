54946dbe53920dee5ea5a32b0a6579b15307363a
monty <velimir.vesselinov@gmail.com>
2023-04-20 08:36:59 -0600

    minor

--- src/MadsPublish.jl
+++ src/MadsPublish.jl
@@ -11,18 +11,7 @@ function documentation_create(modules_doc=madsmodulesdoc, modules_load=string.(m
 	for i in modules_load
 		Core.eval(Main, :(@Mads.tryimportmain $(Symbol(i))))
 	end
-	Documenter.makedocs(root=joinpath(dirname(pathof(Mads)), "..", "docs"), sitename="Mads documentation", format=DocumenterMarkdown.Markdown(), doctest=false, modules=modules_doc, clean=true)
-	current_dir = pwd()
-	cd(Mads.dir)
-	# run(`git pull gh gh-pages`)
-	if Mads.madspython
-		@info("Build documentation ...")
-		run(`python -m mkdocs build --clean`)
-		@info("Done.")
-	else
-		@info("Python and mkdocs might be not available!")
-	end
-	cd(current_dir)
+	Documenter.makedocs(; root=joinpath(Mads.dir, "docs"), sitename="Mads", format=Documenter.Writers.HTMLWriter.HTML(; prettyurls=false, sidebar_sitename=false, assets=["assets/favicon.ico"]), doctest=true, modules=modules_doc, clean=true)
 	return nothing
 end
 
@@ -35,17 +24,6 @@ function documentation_deploy(modules_doc=madsmodulesdoc, modules_load=string.(m
 	for i in modules_load
 		Core.eval(Main, :(@Mads.tryimportmain $(Symbol(i))))
 	end
-	Documenter.makedocs(root=joinpath(dirname(pathof(Mads)), "..", "docs"), sitename="Mads documentation", format=DocumenterMarkdown.Markdown(), doctest=false, modules=modules_doc, clean=true)
-	current_dir = pwd()
-	cd(Mads.dir)
-	# run(`git pull gh gh-pages`)
-	if Mads.madspython
-		@info("Build & deploy documentation ...")
-		run(`python -m mkdocs gh-deploy --clean`)
-		@info("mkdocs done.")
-	else
-		@info("Python and mkdocs might be not available!")
-	end
-	cd(current_dir)
+	Documenter.deploydocs(; root=joinpath(Mads.dir, "docs"), repo="github.com/madsjulia/Mads.jl.git")
 	return nothing
 end
