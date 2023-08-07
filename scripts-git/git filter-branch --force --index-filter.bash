daf54bbb14fdcd5841495583662a7108c6169464
monty <velimir.vesselinov@gmail.com>
2023-08-07 14:38:59 -0600

    filter-repo

---
+++ scripts-git/git filter-branch --force --index-filter.bash
@@ -0,0 +1,2 @@
+git filter-repo --invert-paths --path-glob "examples/model_coupling/external-linearmodel+template+instruction+path/*.svg"
+git filter-repo --invert-paths --path-glob "examples/models/internal-linear-model/*.svg"
