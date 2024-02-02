380a8ad538bc0fa84f3ee89bdadc8345b77249d9
monty <velimir.vesselinov@gmail.com>
2024-02-01 17:49:48 -0700

    work

--- examples/parallel/distributed_test.jl
+++ examples/parallel/distributed_test.jl
@@ -1,12 +1,19 @@
-Distributed.@everywhere md_pta = $md_pta
-Distributed.@everywhere f = Mads.makearrayfunction(md_pta)
-Distributed.@everywhere @show f(rand(23))
+import Distributed
+Distributed.addprocs(4)
 
-Distributed.@everywhere @show myid()
+Distributed.@everywhere @show Distributed.myid()
 import SharedArrays
-sa = SharedArrays.SharedArray{Float64}(23, 5)
-sa = zeros(23, 5)
+si = SharedArrays.SharedArray{Float64,2}((5, 23))
+so = SharedArrays.SharedArray{Float64,2}((5, 8))
+
+sil = Array{Float64,2}(undef, 5, 23)
+sil = rand(5, 23)
+
+sol = Array{Float64,2}(undef, 5, 8)
+sol .= 0
+
+@Distributed.everywhere function_test(x) = rand(8) .+ sum(x)
 
 @sync @Distributed.distributed for i = 1:5
-	sa[:, i] .= rand(23) .+ 2
+	so[i, :] .= function_test(sil[i, :])
 end
