84e7d34051a7f25f2e4d0f5cc3e1d625ace1507b
monty <velimir.vesselinov@gmail.com>
2024-03-12 16:04:40 -0600

    work

--- scripts/stipple.jl
+++ scripts/stipple.jl
@@ -1,8 +1,8 @@
 import Stipple
 import StipplePlotly
 
-@Stipple.reactive! mutable struct UIModelMap <: Stipple.ReactiveModel
-	data::Stipple.R{Vector{StipplePlotly.PlotData}} = [
+@Stipple.vars UIModelMap begin
+	data::Vector{StipplePlotly.PlotData} = [
 		StipplePlotly.PlotData(
 			plot = StipplePlotly.Charts.PLOT_TYPE_SCATTERMAPBOX,
 			lon = [-74, -70, -70, -74],
@@ -16,13 +16,13 @@ import StipplePlotly
 			marker = StipplePlotly.PlotDataMarker(size=10, color="orange")
 		)
 	]
-	layout::Stipple.R{StipplePlotly.PlotLayout} = StipplePlotly.PlotLayout(
+	layout::StipplePlotly.PlotLayout = StipplePlotly.PlotLayout(
 		mapbox = StipplePlotly.PlotLayoutMapbox(; style="stamen-terrain", zoom=5, center=StipplePlotly.MCenter(-73, 46)),
 		showlegend= false,
 		height= 450,
 		width= 600
 	)
-	config::Stipple.R{StipplePlotly.PlotConfig} = StipplePlotly.PlotConfig()
+	config::StipplePlotly.PlotConfig = StipplePlotly.PlotConfig()
 end
 
 ui_model_map = UIModelMap |> Stipple.init
