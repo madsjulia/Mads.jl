d188a53b409bd804a6399426a3151e2540d57a26
monty <velimir.vesselinov@gmail.com>
2024-05-19 12:31:37 -0600

    minor

--- examples/learn_julia/df_test.jl
+++ examples/learn_julia/df_test.jl
@@ -1,5 +1,5 @@
 import DataFrames # load required package
-df = DataFrames.DataFrame(A=1:4,B="a") # create data frame with two columns; scalars are expanded
+df = DataFrames.DataFrame(A=1:4, B="a") # create data frame with two columns; scalars are expanded
 df[!, :C] = repeat([true, false], 2) # OK, new column added
 
 for i in axes(df, 1)
