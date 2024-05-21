3dc9744dec37094c5f83cd458180ebb1fb00eab2
monty <velimir.vesselinov@gmail.com>
2024-05-21 09:52:47 -0600

    apply_xlsx_format

--- src/MadsXLSX.jl
+++ src/MadsXLSX.jl
@@ -1,11 +1,11 @@
 import XLSX
 import DataFrames
 
-function set_xlsx_formats(wf::XLSX.XLSXFile, dec_digits::Integer, exp_digits::Integer)
+function set_xlsx_formats(wf::XLSX.XLSXFile, dec_digits::Integer=4, exp_digits::Integer=2)
 	set_xlsx_formats(wf.workbook, dec_digits, exp_digits)
 end
 
-function set_xlsx_formats(wb::XLSX.Workbook, dec_digits::Integer, exp_digits::Integer)
+function set_xlsx_formats(wb::XLSX.Workbook, dec_digits::Integer=4, exp_digits::Integer=2)
 	dec_digits_string = lpad("", dec_digits, "0")
 	exp_digits_string = lpad("", exp_digits, "0")
 	numfmt_dec = XLSX.styles_add_numFmt(wb, "0.$(dec_digits_string);-0.$(dec_digits_string);-")
@@ -15,11 +15,17 @@ function set_xlsx_formats(wb::XLSX.Workbook, dec_digits::Integer, exp_digits::In
 	return numfmt_style_dec, numfmt_style_exp
 end
 
-function apply_xlsx_format(df::DataFrames.DataFrame, format::XLSX.CellDataFormat, cols::AbstractVector=names(df))
-	DataFrames.select(df, DataFrames.All(), cols .=> DataFrames.ByRow(x -> XLSX.CellValue(x, format)) .=> cols)
+function apply_xlsx_format!(wf::XLSX.XLSXFile, format::XLSX.CellDataFormat=first(set_xlsx_formats(wf.workbook)))
+	apply_xlsx_format(ws.workbook, format)
 end
 
-function apply_xlsx_format(ws::XLSX.Worksheet, format::XLSX.CellDataFormat, cols::AbstractVector=Symbol.(vec(ws[1, :])))
+function apply_xlsx_format!(wb::XLSX.Workbook, format::XLSX.CellDataFormat=first(set_xlsx_formats(wb)))
+	for ws in wb.sheets
+		apply_xlsx_format(ws, format)
+	end
+end
+
+function apply_xlsx_format!(ws::XLSX.Worksheet, format::XLSX.CellDataFormat, cols::AbstractVector=Symbol.(vec(ws[1, :])))
 	for j in axes(ws, 2)
 		if Symbol(ws[1, j]) in cols
 			for i in axes(ws, 1)
@@ -29,4 +35,12 @@ function apply_xlsx_format(ws::XLSX.Worksheet, format::XLSX.CellDataFormat, cols
 			end
 		end
 	end
+end
+
+function apply_xlsx_format!(df::DataFrames.DataFrame, format::XLSX.CellDataFormat, cols::AbstractVector=names(df))
+	DataFrames.select!(df, DataFrames.All(), cols .=> DataFrames.ByRow(x -> XLSX.CellValue(x, format)) .=> cols)
+end
+
+function apply_xlsx_format(df::DataFrames.DataFrame, format::XLSX.CellDataFormat, cols::AbstractVector=names(df))
+	return DataFrames.select(df, DataFrames.All(), cols .=> DataFrames.ByRow(x -> XLSX.CellValue(x, format)) .=> cols)
 end
