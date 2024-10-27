915ae6637de83ed54d7b6ac54d05afab0f22c6ea
Velimir (monty) Vesselinov <velimir.vesselinov@gmail.com>
2024-10-27 15:50:58 -0600

    Float32

--- examples/xlsx/xlsx.jl
+++ examples/xlsx/xlsx.jl
@@ -17,6 +17,8 @@ df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=1:6)
 
 df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5])
 
+df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5], numbertype=Float32)
+
 dd = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5], dataframe=false)
 
 dd = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5], dataframe=false, keytype=Symbol)
