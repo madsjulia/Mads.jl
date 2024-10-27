9e02889e3b1c06cc5039cc82d4c9821a3a66d002
Velimir (monty) Vesselinov <velimir.vesselinov@gmail.com>
2024-10-27 15:49:35 -0600

    work

--- examples/xlsx/xlsx.jl
+++ examples/xlsx/xlsx.jl
@@ -1,15 +1,22 @@
 import Mads
+import DataFrames
+import OrderedCollections
 
-cd(@__DIR__)
+workdir = joinpath(Mads.dir, "examples", "xlsx")
+xlsxfile = joinpath(workdir, "test.xlsx")
 
-df = Mads.get_excel_data("test.xlsx")
+df = Mads.get_excel_data(xlsxfile)
 
-df = Mads.get_excel_data("test.xlsx", "Sheet1")
+df = Mads.get_excel_data(xlsxfile, "Sheet1")
 
-df = Mads.get_excel_data("test.xlsx", "Sheet1"; header=1:2)
+df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:2)
 
-df = Mads.get_excel_data("test.xlsx", "Sheet1"; header=1:1, rows=3:102)
+df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102)
 
-df = Mads.get_excel_data("test.xlsx", "Sheet1"; header=1:1, rows=3:102, cols=1:6)
+df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=1:6)
 
-df = Mads.get_excel_data("test.xlsx", "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5])
+df = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5])
+
+dd = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5], dataframe=false)
+
+dd = Mads.get_excel_data(xlsxfile, "Sheet1"; header=1:1, rows=3:102, cols=[1,3,7,5], dataframe=false, keytype=Symbol)
