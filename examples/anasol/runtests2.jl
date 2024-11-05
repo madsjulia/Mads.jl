df0b3075d4aa69446487c1f9e6a450e7991f7edc
monty <velimir.vesselinov@gmail.com>
2024-11-05 11:52:27 -1000

    reformat

--- examples/anasol/runtests2.jl
+++ examples/anasol/runtests2.jl
@@ -7,7 +7,7 @@ workdir = joinpath(Mads.dir, "examples", "anasol")
 md = Mads.loadmadsfile(joinpath(workdir, "w01pure.mads"))
 ns = 10000
 rsetdict = Mads.getparamrandom(md, ns)
-rsetarray = hcat(map(i->rsetdict[i], keys(rsetdict))...)'
+rsetarray = hcat(map(i -> rsetdict[i], keys(rsetdict))...)'
 rsetarrayplus = [repmat([823], ns)'; repmat([1499], ns)'; repmat([3], ns)'; rsetarray; repmat([15], ns)']
 Mads.vectoron()
 computeconcentrations = Mads.makedoublearrayfunction(md)
@@ -35,5 +35,5 @@ end
 
 rvd = Array{Float64}(undef, ns);
 @time for i = 1:ns
-	rvd[i] = Mads.contamination(823., 1499., 3., rsetarray[:, i]..., 15., Anasol.long_fff_bbb_iir_c)
+	rvd[i] = Mads.contamination(823.0, 1499.0, 3.0, rsetarray[:, i]..., 15.0, Anasol.long_fff_bbb_iir_c)
 end
