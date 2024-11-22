c1d4ddfbcb8ac62c92c6e3897066be59d7c448b4
monty <velimir.vesselinov@gmail.com>
2024-11-22 10:01:38 -0700

    links

--- examples/model_coupling/external-linearmodel-jld.jl
+++ examples/model_coupling/external-linearmodel-jld.jl
@@ -1,10 +1,1 @@
-import JLD
-import OrderedCollections
-
-parameters = JLD.load("parameters.jld") # JLD file created to write current model parameters
-JLD
-f(t) = parameters["a"] * t - parameters["b"] # a * t - b; linear model
-times = 1:4
-predictions = OrderedCollections.OrderedDict(zip(map(i -> string("o", i), times), map(f, times)))
-
-JLD.save("predictions.jld", predictions) # JLD file created to write current model predictions
+../models/external-linear-model/external-linearmodel-jld.jl
--- examples/model_coupling/external-linearmodel-jld2.jl
+++ examples/model_coupling/external-linearmodel-jld2.jl
@@ -1,10 +1,1 @@
-import JLD2
-import OrderedCollections
-
-parameters = JLD2.load("parameters.jld2") # JLD file created to write current model parameters
-
-f(t) = parameters["a"] * t - parameters["b"] # a * t - b; linear model
-times = 1:4
-predictions = OrderedCollections.OrderedDict(zip(map(i -> string("o", i), times), map(f, times)))
-
-JLD2.save("predictions.jld2", predictions) # JLD file created to write current model predictions
+../models/external-linear-model/external-linearmodel-jld2.jl
